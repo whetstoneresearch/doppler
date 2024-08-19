0cd0f4b86d0911a9a17f3555c00bacaea6b5dfd6
kadenzipfel <kaden.zipfel@hotmail.com>
2024-08-19 14:03:05 -0700

    Add state

--- src/Doppler.sol
+++ src/Doppler.sol
@@ -8,7 +8,45 @@ import {PoolKey} from "v4-periphery/lib/v4-core/src/types/PoolKey.sol";
 import {BeforeSwapDelta, BeforeSwapDeltaLibrary} from "v4-periphery/lib/v4-core/src/types/BeforeSwapDelta.sol";
 
 contract Doppler is BaseHook {
-    constructor(IPoolManager _poolManager) BaseHook(_poolManager) {}
+    // TODO: consider if we can use smaller uints
+    struct State {
+        uint40 lastEpoch; // last updated epoch
+        uint256 tickAccumulator; // accumulator to modify the bonding curve
+        uint256 totalTokensSold; // total tokens sold
+        uint256 totalProceeds; // total amount earned from selling tokens
+        uint256 soldInLatestEpoch; // amount of tokens sold in the latest epoch
+    }
+
+    // TODO: consider whether this needs to be public
+    State public state;
+
+    // TODO: totalLiquidity?
+    uint256 immutable startingLiquidity; // starting amount of tokens to be sold
+    uint256 immutable startingTime; // sale start time
+    uint256 immutable endingTime; // sale end time
+    int24 immutable startingTick; // dutch auction starting tick
+    int24 immutable endingTick; // dutch auction ending tick
+    uint256 immutable epochLength; // length of each epoch (seconds)
+    uint256 immutable gamma; // 1.0001 ** (gamma) = max single block increase
+
+    constructor(
+        IPoolManager _poolManager,
+        uint256 _startingLiquidity,
+        uint256 _startingTime,
+        uint256 _endingTime,
+        int24 _startingTick,
+        int24 _endingTick,
+        uint256 _epochLength,
+        uint256 _gamma
+    ) BaseHook(_poolManager) {
+        startingLiquidity = _startingLiquidity;
+        startingTime = _startingTime;
+        endingTime = _endingTime;
+        startingTick = _startingTick;
+        endingTick = _endingTick;
+        epochLength = _epochLength;
+        gamma = _gamma;
+    }
 
     function beforeSwap(address, PoolKey calldata, IPoolManager.SwapParams calldata, bytes calldata)
         external
