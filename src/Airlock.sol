b2171ed2703800f694421725a9cbdfcebd983209
clemlak <clementlakhal@gmail.com>
2024-09-12 16:31:05 +0400

    feat: update create, add Token struct

--- src/Airlock.sol
+++ src/Airlock.sol
@@ -14,16 +14,17 @@ enum FactoryState {
 
 error WrongFactoryState();
 
-enum TokenState {
-    Created,
-    Migrated
+struct Token {
+    address governance;
+    bool hasMigrated;
+    address hook;
 }
 
 contract Airlock is Ownable {
     PoolManager public immutable poolManager;
 
     mapping(address => FactoryState) public getFactoryState;
-    mapping(address token => address hook) public getHookOf;
+    mapping(address tokenAddress => Token) public getToken;
 
     constructor(PoolManager poolManager_) {
         poolManager = poolManager_;
@@ -41,28 +42,32 @@ contract Airlock is Ownable {
         address tokenFactory,
         address governanceFactory,
         address liquidityFactory,
-        address stageAdmin,
         uint256 liquidityAmount,
+        address stageAdmin,
         address[] memory recipients,
         uint256[] memory amounts,
-        bytes memory tokenInfo
+        string memory name,
+        string memory symbol,
+        bytes memory hookData
     ) external returns (address tokenAddress) {
         require(getFactoryState[tokenFactory] == FactoryState.TokenFactory, WrongFactoryState());
         require(getFactoryState[governanceFactory] == FactoryState.GovernanceFactory, WrongFactoryState());
         require(getFactoryState[liquidityFactory] == FactoryState.LiquidityFactory, WrongFactoryState());
-        tokenAddress = TokenFactory(tokenFactory).create("", "", 1_000_000_000e18, msg.sender);
-        getHookOf[tokenAddress] = liquidityFactory;
-    }
+        tokenAddress = TokenFactory(tokenFactory).create(name, symbol, 1_000_000_000e18, msg.sender); // TODO: Define the total supply
 
-    // TODO: I think this function be just be called directly by `create`
-    function stageLiquidity(address token, bytes memory hookData) external {
+        getToken[tokenAddress] = Token({
+            governance: address(0), // TODO: Deploy a minimal governance contract
+            hasMigrated: false,
+            hook: liquidityFactory
+        });
+
         PoolKey memory key = PoolKey({
             // TODO: Currently only ETH pairs are supported
             currency0: Currency.wrap(address(0)),
-            currency1: Currency.wrap(token),
+            currency1: Currency.wrap(tokenAddress),
             fee: 0, // TODO: Do we want users to have the ability to set the fee?
             tickSpacing: 60, // TODO: Do we want users to have the ability to set the tickSpacing?
-            hooks: IHooks(getHookOf[token])
+            hooks: IHooks(liquidityFactory)
         });
 
         uint160 sqrtPriceX96;
