9b01453568f6a3a7b03f269eb371334bfc949346
Clément Lakhal <39790678+clemlak@users.noreply.github.com>
2024-09-23 21:46:38 +0400

    Feat/callback (#52)
    * feat: add unlockCallback
    
    * test: add test_unlockCallback_RevertsWhenNotPoolManager
    
    * test: add public unlock function
    
    * test: add test_unlockCallback_SucceedWhenSenderIsPoolManager
    
    * feat: move back _update logic into its function
    
    * feat: wip callback
    
    * test: remove old parameters of Doppler.CallbackData in callback test
    
    * feat: use transfer and track deltas in unlock callback
    
    * test: add tick param to CallbackData
    
    * feat: fix _getCurrentEpoch before startingTime, add too many logs
    
    * fix computeUpperSlug
    
    * first pass at fixing slugs
    
    * account for rounding when aligning computed ticks
    
    * cleanup console logs, fix gamma share function
    
    * fix kaden comments
    
    * cache positions in unlockCallback
    
    ---------
    
    Co-authored-by: Kinrezc <matt.czernik@gmail.com>

---
+++ test/UnlockCallback.t.sol
@@ -0,0 +1,21 @@
+/// SPDX-License-Identifier: UNLICENSED
+pragma solidity ^0.8.13;
+
+import {console} from "forge-std/console.sol";
+import {SafeCallback} from "v4-periphery/src/base/SafeCallback.sol";
+import {BaseTest} from "test/BaseTest.sol";
+import {Doppler} from "src/Doppler.sol";
+
+contract testUnlockCallbackTest is BaseTest {
+    function test_unlockCallback_RevertsWhenNotPoolManager() public {
+        vm.expectRevert(SafeCallback.NotPoolManager.selector);
+        ghost().hook.unlockCallback("");
+    }
+
+    function test_unlockCallback_SucceedWhenSenderIsPoolManager() public {
+        Doppler.CallbackData memory callbackData =
+            Doppler.CallbackData({key: ghost().key(), tick: ghost().hook.getStartingTick(), sender: address(0xbeef)});
+        vm.prank(address(manager));
+        ghost().hook.unlock(abi.encode(callbackData));
+    }
+}
