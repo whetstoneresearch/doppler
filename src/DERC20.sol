ca00fab7da3c89db760345e62d828bf2cf1fd060
clemlak <clementlakhal@gmail.com>
2024-09-26 12:13:39 +0400

    feat: add minting start date, inherit Ownable

--- src/DERC20.sol
+++ src/DERC20.sol
@@ -4,17 +4,21 @@ pragma solidity ^0.8.13;
 import {ERC20} from "openzeppelin/token/ERC20/ERC20.sol";
 import {ERC20Votes} from "openzeppelin/token/ERC20/extensions/ERC20Votes.sol";
 import {EIP712} from "openzeppelin/utils/cryptography/EIP712.sol";
+import {Ownable} from "openzeppelin/access/Ownable.sol";
 
 /**
  * TODO:
  * - Add mint cap: bounded annual max inflation which can only go down
  * - Governor can only start minting after 365 days
- * - Fee on transfer
  */
-contract DERC20 is ERC20Votes {
+error MintingNotStartedYet();
+
+contract DERC20 is ERC20Votes, Ownable {
     uint256 public feeOnTransfer;
     mapping(address => bool) public isExemptFromFees;
     address public feeCollector;
+
+    uint256 public immutable mintStartDate;
 
     constructor(
         string memory name_,
@@ -23,8 +27,9 @@ contract DERC20 is ERC20Votes {
         address recipient,
         uint256 feeOnTransfer_,
         address[] memory exemptFromFees,
-        address feeCollector_
-    ) EIP712(name_, symbol_) ERC20(name_, symbol_) {
+        address feeCollector_,
+        address owner_
+    ) EIP712(name_, symbol_) ERC20(name_, symbol_) Ownable(owner_) {
         _mint(recipient, totalSupply_);
         feeOnTransfer = feeOnTransfer_;
 
@@ -34,6 +39,13 @@ contract DERC20 is ERC20Votes {
 
         feeCollector = feeCollector_;
         isExemptFromFees[feeCollector_] = true; // Just in case
+
+        mintStartDate = block.timestamp + 365 days;
+    }
+
+    function mint(address to, uint256 value) external onlyOwner {
+        require(block.timestamp >= mintStartDate, MintingNotStartedYet());
+        _mint(to, value);
     }
 
     function _update(address from, address to, uint256 value) internal virtual override {
