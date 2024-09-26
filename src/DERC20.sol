21f382623744f611a8f606fad9c41f9555ebf82c
clemlak <clementlakhal@gmail.com>
2024-09-26 11:50:05 +0400

    feat: inherit from ERC20Votes

--- src/DERC20.sol
+++ src/DERC20.sol
@@ -2,6 +2,8 @@
 pragma solidity ^0.8.13;
 
 import {ERC20} from "openzeppelin/token/ERC20/ERC20.sol";
+import {ERC20Votes} from "openzeppelin/token/ERC20/extensions/ERC20Votes.sol";
+import {EIP712} from "openzeppelin/utils/cryptography/EIP712.sol";
 
 /**
  * TODO:
@@ -9,8 +11,9 @@ import {ERC20} from "openzeppelin/token/ERC20/ERC20.sol";
  * - Governor can only start minting after 365 days
  * - Fee on transfer
  */
-contract DERC20 is ERC20 {
+contract DERC20 is ERC20Votes {
     constructor(string memory name_, string memory symbol_, uint256 totalSupply_, address recipient)
+        EIP712(name_, symbol_)
         ERC20(name_, symbol_)
     {
         _mint(recipient, totalSupply_);
