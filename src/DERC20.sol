9e14a6b6f4914b4d5bcde40ca2b84235ab7160e9
clemlak <clementlakhal@gmail.com>
2024-09-26 12:09:51 +0400

    feat: add fee on transfer

--- src/DERC20.sol
+++ src/DERC20.sol
@@ -12,10 +12,37 @@ import {EIP712} from "openzeppelin/utils/cryptography/EIP712.sol";
  * - Fee on transfer
  */
 contract DERC20 is ERC20Votes {
-    constructor(string memory name_, string memory symbol_, uint256 totalSupply_, address recipient)
-        EIP712(name_, symbol_)
-        ERC20(name_, symbol_)
-    {
+    uint256 public feeOnTransfer;
+    mapping(address => bool) public isExemptFromFees;
+    address public feeCollector;
+
+    constructor(
+        string memory name_,
+        string memory symbol_,
+        uint256 totalSupply_,
+        address recipient,
+        uint256 feeOnTransfer_,
+        address[] memory exemptFromFees,
+        address feeCollector_
+    ) EIP712(name_, symbol_) ERC20(name_, symbol_) {
         _mint(recipient, totalSupply_);
+        feeOnTransfer = feeOnTransfer_;
+
+        for (uint256 i = 0; i < exemptFromFees.length; i++) {
+            isExemptFromFees[exemptFromFees[i]] = true;
+        }
+
+        feeCollector = feeCollector_;
+        isExemptFromFees[feeCollector_] = true; // Just in case
+    }
+
+    function _update(address from, address to, uint256 value) internal virtual override {
+        super._update(from, to, value);
+
+        // TODO: Check when should we charge / NOT charge the fee?
+        if (feeOnTransfer > 0 && !isExemptFromFees[from] && !isExemptFromFees[to]) {
+            uint256 fee = feeOnTransfer * value / 10_000;
+            _transfer(to, feeCollector, fee);
+        }
     }
 }
