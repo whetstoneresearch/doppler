c1d16728a7e507ab466c608668d05f372eb7f6dc
clemlak <clementlakhal@gmail.com>
2024-09-27 19:08:03 +0400

    feat: pass name parameter to Governance

--- src/GovernanceFactory.sol
+++ src/GovernanceFactory.sol
@@ -6,11 +6,11 @@ import {TimelockController} from "openzeppelin/governance/TimelockController.sol
 import {IGovernanceFactory} from "src/interfaces/IGovernanceFactory.sol";
 
 contract GovernanceFactory is IGovernanceFactory {
-    function create(address token, bytes memory)
+    function create(string memory name, address token, bytes memory)
         external
         returns (address governance, TimelockController timelockController)
     {
         timelockController = new TimelockController(2 days, new address[](0), new address[](0), address(0));
-        governance = address(new Governance(IVotes(token), TimelockController(timelockController)));
+        governance = address(new Governance(name, IVotes(token), TimelockController(timelockController)));
     }
 }
