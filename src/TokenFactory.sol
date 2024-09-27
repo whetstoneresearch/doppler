f479a3a7bcdf42d838e3e1099913745203e50a40
clemlak <clementlakhal@gmail.com>
2024-09-27 12:17:53 +0400

    feat: inherit ITokenFactory

--- src/TokenFactory.sol
+++ src/TokenFactory.sol
@@ -1,19 +1,21 @@
 /// SPDX-License-Identifier: UNLICENSED
 pragma solidity ^0.8.13;
 
+import {ITokenFactory} from "src/interfaces/ITokenFactory.sol";
 import {DERC20} from "src/DERC20.sol";
 
-contract TokenFactory {
+contract TokenFactory is ITokenFactory {
     function create(
         string memory name,
         string memory symbol,
         uint256 totalSupply,
         address recipient,
-        uint256 feeOnTransfer,
-        address[] memory exemptFromFees,
-        address feeCollector,
-        address owner
+        address owner,
+        bytes memory tokenData
     ) external returns (address) {
+        (uint256 feeOnTransfer, address[] memory exemptFromFees, address feeCollector) =
+            abi.decode(tokenData, (uint256, address[], address));
+
         return address(
             new DERC20(name, symbol, totalSupply, recipient, feeOnTransfer, exemptFromFees, feeCollector, owner)
         );
