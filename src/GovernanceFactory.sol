ec1af5555771726ed8452b43b19efff09cc26d20
clemlak <clementlakhal@gmail.com>
2024-09-27 11:57:33 +0400

    feat: inherit from IGovernanceFactory

--- src/GovernanceFactory.sol
+++ src/GovernanceFactory.sol
@@ -3,9 +3,14 @@ pragma solidity ^0.8.13;
 
 import {Governance, IVotes} from "src/Governance.sol";
 import {TimelockController} from "openzeppelin/governance/TimelockController.sol";
+import {IGovernanceFactory} from "src/interfaces/IGovernanceFactory.sol";
 
-contract GovernanceFactory {
-    function create(address token) external returns (address governance, TimelockController timelockController) {
+contract GovernanceFactory is IGovernanceFactory {
+    function create(bytes memory governanceData)
+        external
+        returns (address governance, TimelockController timelockController)
+    {
+        address token = abi.decode(governanceData, (address));
         timelockController = new TimelockController(2 days, new address[](0), new address[](0), address(0));
         governance = address(new Governance(IVotes(token), TimelockController(timelockController)));
     }
