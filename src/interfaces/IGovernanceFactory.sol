ea1e4ab918b122fd8427498061c25c33d7e3d762
clemlak <clementlakhal@gmail.com>
2024-09-27 11:57:26 +0400

    feat: add IGovernanceFactory

---
+++ src/interfaces/IGovernanceFactory.sol
@@ -0,0 +1,11 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity ^0.8.13;
+
+import {TimelockController} from "openzeppelin/governance/TimelockController.sol";
+import {IGovernanceFactory} from "src/interfaces/IGovernanceFactory.sol";
+
+interface IGovernanceFactory {
+    function create(bytes memory governanceData)
+        external
+        returns (address governance, TimelockController timelockController);
+}
