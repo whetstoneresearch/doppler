c3bbf3008b3863610eb78bd2f49e1a7a8d792b0d
clemlak <clementlakhal@gmail.com>
2024-09-27 12:27:44 +0400

    feat: add token default parameter

--- src/GovernanceFactory.sol
+++ src/GovernanceFactory.sol
@@ -6,11 +6,10 @@ import {TimelockController} from "openzeppelin/governance/TimelockController.sol
 import {IGovernanceFactory} from "src/interfaces/IGovernanceFactory.sol";
 
 contract GovernanceFactory is IGovernanceFactory {
-    function create(bytes memory governanceData)
+    function create(address token, bytes memory)
         external
         returns (address governance, TimelockController timelockController)
     {
-        address token = abi.decode(governanceData, (address));
         timelockController = new TimelockController(2 days, new address[](0), new address[](0), address(0));
         governance = address(new Governance(IVotes(token), TimelockController(timelockController)));
     }
