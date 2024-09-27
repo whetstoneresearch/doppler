0429375cc10577b292edd65139323d28eab14f70
clemlak <clementlakhal@gmail.com>
2024-09-27 11:38:21 +0400

    feat: update TokenFactory

--- src/TokenFactory.sol
+++ src/TokenFactory.sol
@@ -4,10 +4,18 @@ pragma solidity ^0.8.13;
 import {DERC20} from "src/DERC20.sol";
 
 contract TokenFactory {
-    function create(string memory name, string memory symbol, uint256 totalSupply, address recipient)
-        external
-        returns (address)
-    {
-        return address(new DERC20(name, symbol, totalSupply, recipient));
+    function create(
+        string memory name,
+        string memory symbol,
+        uint256 totalSupply,
+        address recipient,
+        uint256 feeOnTransfer,
+        address[] memory exemptFromFees,
+        address feeCollector,
+        address owner
+    ) external returns (address) {
+        return address(
+            new DERC20(name, symbol, totalSupply, recipient, feeOnTransfer, exemptFromFees, feeCollector, owner)
+        );
     }
 }
