d166531f13e5f1cec8febe00ef2820f6dc14f949
clemlak <clementlakhal@gmail.com>
2024-09-30 15:17:57 +0400

    feat: add predict to IHookFactory

--- src/interfaces/IHookFactory.sol
+++ src/interfaces/IHookFactory.sol
@@ -4,5 +4,9 @@ pragma solidity ^0.8.13;
 import {IPoolManager} from "v4-core/src/interfaces/IPoolManager.sol";
 
 interface IHookFactory {
-    function create(IPoolManager poolManager, bytes memory hookData) external returns (address);
+    function create(IPoolManager poolManager, bytes memory hookData, bytes32 salt) external returns (address);
+    function predict(IPoolManager poolManager, bytes memory hookData)
+        external
+        view
+        returns (address hookAddress, bytes32 salt);
 }
