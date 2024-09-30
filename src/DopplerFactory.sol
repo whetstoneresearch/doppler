fcdd062c89098b250995f7f0dd1dcc8fb75c6965
clemlak <clementlakhal@gmail.com>
2024-09-30 15:18:10 +0400

    feat: add predict to DopplerFactory

--- src/DopplerFactory.sol
+++ src/DopplerFactory.sol
@@ -8,7 +8,7 @@ import {HookMiner} from "src/HookMiner.sol";
 import {Hooks} from "v4-core/src/libraries/Hooks.sol";
 
 contract DopplerFactory is IHookFactory {
-    function create(IPoolManager poolManager, bytes memory hookData) external returns (address) {
+    function create(IPoolManager poolManager, bytes memory hookData, bytes32 salt) external returns (address) {
         (
             uint256 numTokensToSell,
             uint256 startingTime,
@@ -20,7 +20,38 @@ contract DopplerFactory is IHookFactory {
             bool isToken0
         ) = abi.decode(hookData, (uint256, uint256, uint256, int24, int24, uint256, uint256, bool));
 
-        (address hookAddress, bytes32 salt) = HookMiner.find(
+        return address(
+            new Doppler{salt: salt}(
+                poolManager,
+                numTokensToSell,
+                startingTime,
+                endingTime,
+                startingTick,
+                endingTick,
+                epochLength,
+                gamma,
+                isToken0
+            )
+        );
+    }
+
+    function predict(IPoolManager poolManager, bytes memory hookData)
+        public
+        view
+        returns (address hookAddress, bytes32 salt)
+    {
+        (
+            uint256 numTokensToSell,
+            uint256 startingTime,
+            uint256 endingTime,
+            int24 startingTick,
+            int24 endingTick,
+            uint256 epochLength,
+            uint256 gamma,
+            bool isToken0
+        ) = abi.decode(hookData, (uint256, uint256, uint256, int24, int24, uint256, uint256, bool));
+
+        (hookAddress, salt) = HookMiner.find(
             address(this),
             uint160(Hooks.BEFORE_ADD_LIQUIDITY_FLAG | Hooks.BEFORE_SWAP_FLAG | Hooks.AFTER_SWAP_FLAG),
             type(Doppler).creationCode,
@@ -36,19 +67,5 @@ contract DopplerFactory is IHookFactory {
                 isToken0
             )
         );
-
-        new Doppler{salt: salt}(
-            poolManager,
-            numTokensToSell,
-            startingTime,
-            endingTime,
-            startingTick,
-            endingTick,
-            epochLength,
-            gamma,
-            isToken0
-        );
-
-        return hookAddress;
     }
 }
