16a1ef2396599749ab5ed78eb718560cb01412ef
clemlak <clementlakhal@gmail.com>
2024-10-02 14:52:01 +0400

    feat: change default HookFactory parameters

--- src/interfaces/IHookFactory.sol
+++ src/interfaces/IHookFactory.sol
@@ -4,9 +4,29 @@ pragma solidity ^0.8.13;
 import {IPoolManager} from "v4-core/src/interfaces/IPoolManager.sol";
 
 interface IHookFactory {
-    function create(IPoolManager poolManager, bytes memory hookData, bytes32 salt) external returns (address);
-    function predict(IPoolManager poolManager, bytes memory hookData)
-        external
-        view
-        returns (address hookAddress, bytes32 salt);
+    function create(
+        IPoolManager poolManager,
+        uint256 numTokensToSell,
+        uint256 startingTime,
+        uint256 endingTime,
+        int24 startingTick,
+        int24 endingTick,
+        uint256 epochLength,
+        uint256 gamma,
+        bool isToken0,
+        bytes memory hookData,
+        bytes32 salt
+    ) external returns (address);
+    function predict(
+        IPoolManager poolManager,
+        uint256 numTokensToSell,
+        uint256 startingTime,
+        uint256 endingTime,
+        int24 startingTick,
+        int24 endingTick,
+        uint256 epochLength,
+        uint256 gamma,
+        bool isToken0,
+        bytes memory
+    ) external view returns (address hookAddress, bytes32 salt);
 }
