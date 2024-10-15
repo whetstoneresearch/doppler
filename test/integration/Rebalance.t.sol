eb4b8e3d5fe61bdf588bbb5f124db7b746fc56ee
kinrezc <matt.czernik@gmail.com>
2024-10-15 19:37:28 -0400

    remove amount0delta from test (idk how it got there)

--- test/integration/Rebalance.t.sol
+++ test/integration/Rebalance.t.sol
@@ -282,11 +282,6 @@ contract RebalanceTest is BaseTest {
         // TODO: Figure out how this can possibly fail even though the following trade succeeds
         assertGt(amount0Delta, totalTokensSold);
 
-        // assert that the lowerSlug can support the purchase of 99.9% of the tokens sold
-        assertApproxEqAbs(amount0Delta, totalTokensSold, totalTokensSold * 1 / 1000);
-        // TODO: Figure out how this can possibly fail even though the following trade succeeds
-        assertGt(amount0Delta, totalTokensSold);
-
         // Validate that we can swap all tokens back into the curve
         swapRouter.swap(
             // Swap asset to numeraire
