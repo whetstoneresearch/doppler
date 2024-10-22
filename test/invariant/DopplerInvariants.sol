e925a5a6989185eca118015624c0eadd5dcd5192
clemlak <clementlakhal@gmail.com>
2024-10-22 21:11:59 +0400

    test: update invariant tests setup

--- test/invariant/DopplerInvariants.sol
+++ test/invariant/DopplerInvariants.sol
@@ -1,16 +1,32 @@
 // SPDX-License-Identifier: UNLICENSED
 pragma solidity ^0.8.13;
 
+import {console} from "forge-std/console.sol";
 import {BaseTest} from "test/shared/BaseTest.sol";
+import {DopplerHandler} from "test/invariant/DopplerHandler.sol";
 
 contract DopplerInvariantsTest is BaseTest {
+    DopplerHandler public handler;
+
     function setUp() public override {
         super.setUp();
+        handler = new DopplerHandler(key, hook, router, isToken0, usingEth);
+
+        bytes4[] memory selectors = new bytes4[](1);
+        selectors[0] = handler.buyExactAmountIn.selector;
+
+        targetSelector(FuzzSelector({addr: address(handler), selectors: selectors}));
+        targetContract(address(handler));
     }
 
-    function afterInvariant() public view {}
+    function afterInvariant() public view {
+        console.log("Handler address", address(handler));
+        console.log("Calls: ", handler.totalCalls());
+        console.log("buyExactAmountIn: ", handler.calls(handler.buyExactAmountIn.selector));
+    }
 
-    function invariant_works() public pure {
+    /// forge-config: default.invariant.fail-on-revert = true
+    function invariant_works() public {
         assertTrue(true);
     }
 }
