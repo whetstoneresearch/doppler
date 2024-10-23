5e3521c2193420a48df0ffca5331042cde0733bd
kinrezc <matt.czernik@gmail.com>
2024-10-23 15:16:25 -0400

    rm console logs

--- test/unit/Swap.sol
+++ test/unit/Swap.sol
@@ -17,7 +17,7 @@ import {
     InvalidSwapAfterMaturitySufficientProceeds
 } from "src/Doppler.sol";
 import {BaseTest} from "test/shared/BaseTest.sol";
-import "forge-std/console.sol";
+
 contract SwapTest is BaseTest {
     using StateLibrary for IPoolManager;
     using ProtocolFeeLibrary for *;
@@ -182,14 +182,10 @@ contract SwapTest is BaseTest {
     function test_swap_DoesNotRebalanceInTheFirstEpoch() public {
         (, int256 tickAccumulator,,,,) = hook.state();
 
-        console.log("tickAccumulator", tickAccumulator);
-
         vm.warp(hook.getStartingTime());
 
         buy(1 ether);
         (, int256 tickAccumulator2,,,,) = hook.state();
-
-        console.log("tickAccumulator2", tickAccumulator2);
 
         assertEq(tickAccumulator, tickAccumulator2);
     }
