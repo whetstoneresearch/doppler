de26d4842edbf074df6fa3bad4122a11124a12d0
clemlak <clementlakhal@gmail.com>
2024-10-24 10:26:10 +0400

    test: add invariant_PositionsDifferentTicks invariant test

--- test/invariant/DopplerInvariants.sol
+++ test/invariant/DopplerInvariants.sol
@@ -8,6 +8,25 @@ import {State} from "src/Doppler.sol";
 import {LiquidityAmounts} from "v4-core/test/utils/LiquidityAmounts.sol";
 import {TickMath} from "v4-core/src/libraries/TickMath.sol";
 
+/*
+
+ ? totalTokensSold can't underflow
+ ? totalProceeds can't underflow
+ ? Computed ticks can't under/overflow
+ X Amount of asset tokens to be supplied to liquidity positions at once <= numTokensToSell - totalTokensSold
+ Likely further have to consider the actual available token balance as dust is lost due to rounding
+ Does not attempt to create liquidity positions with equal upper and lower ticks
+Leads to divide by zero error (even just in computing the liquidity amount)
+Also relevant for just doing relevant math, e.g. retrieving liquidity given an amount can result in a revert
+ Cannot trade the price to below the lower slug range
+Else it allows for price manipulation
+I think this is also a compliance requirement regardless
+ Always places a lower slug if totalTokensSold > 0
+ Selling all tokens back in to the curve must exceed the available liquidity
+ Single tick ranges do not exceed max liquidity per tick
+ Cannot modify the price in any way prior to the start time
+ */
+
 contract DopplerInvariantsTest is BaseTest {
     DopplerHandler public handler;
 
@@ -28,11 +47,14 @@ contract DopplerInvariantsTest is BaseTest {
     }
 
     function afterInvariant() public view {
-        console.log("Calls: ", handler.totalCalls());
-        console.log("buyExactAmountIn: ", handler.calls(handler.buyExactAmountIn.selector));
-        console.log("buyExactAmountOut: ", handler.calls(handler.buyExactAmountOut.selector));
-        console.log("sellExactIn: ", handler.calls(handler.sellExactIn.selector));
-        console.log("sellExactOut: ", handler.calls(handler.sellExactOut.selector));
+        console.log("+-------------------+-----------------------+");
+        console.log("| Function Name     | Calls                 |", handler.totalCalls());
+        console.log("+-------------------+-----------------------+");
+        console.log("| buyExactAmountIn  |", handler.calls(handler.buyExactAmountIn.selector), "                    |");
+        console.log("| buyExactAmountOut |", handler.calls(handler.buyExactAmountOut.selector), "                    |");
+        console.log("| sellExactIn       |", handler.calls(handler.sellExactIn.selector), "                    |");
+        console.log("| sellExactOut      |", handler.calls(handler.sellExactOut.selector), "                    |");
+        console.log("+-------------------+-----------------------+");
     }
 
     /// forge-config: default.invariant.fail-on-revert = true
@@ -77,6 +99,15 @@ contract DopplerInvariantsTest is BaseTest {
         if (totalTokensSold > 0) {
             (,, uint128 liquidity,) = hook.positions(bytes32(uint256(1)));
             assertTrue(liquidity > 0);
+        }
+    }
+
+    /// forge-config: default.invariant.fail-on-revert = true
+    function invariant_PositionsDifferentTicks() public view {
+        uint256 slugs = hook.getNumPDSlugs();
+        for (uint256 i = 1; i < 4 + slugs; i++) {
+            (int24 tickLower, int24 tickUpper, uint128 liquidity,) = hook.positions(bytes32(uint256(i)));
+            if (liquidity > 0) assertTrue(tickLower != tickUpper);
         }
     }
 }
