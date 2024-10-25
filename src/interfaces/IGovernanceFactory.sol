3231b1aade9d83c32ed4ba94ba0b4a6c7068876d
clemlak <clementlakhal@gmail.com>
2024-10-25 19:15:07 +0400

    feat: return address instead of TimelockController type

--- src/interfaces/IGovernanceFactory.sol
+++ src/interfaces/IGovernanceFactory.sol
@@ -1,11 +1,10 @@
 // SPDX-License-Identifier: UNLICENSED
 pragma solidity ^0.8.13;
 
-import {TimelockController} from "@openzeppelin/governance/TimelockController.sol";
 import {IGovernanceFactory} from "src/interfaces/IGovernanceFactory.sol";
 
 interface IGovernanceFactory {
     function create(string memory name, address token, bytes memory governanceData)
         external
-        returns (address governance, TimelockController timelockController);
+        returns (address governance, address timelockController);
 }
