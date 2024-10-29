41ddda5c1872615d4e482bba4965081dbcdca142
clemlak <clementlakhal@gmail.com>
2024-10-29 15:19:02 +0400

    test: add test_setModuleState_RevertsWhenSenderNotOwner

--- test/unit/Airlock.t.sol
+++ test/unit/Airlock.t.sol
@@ -4,6 +4,7 @@ pragma solidity ^0.8.13;
 import {Deployers} from "v4-core/test/utils/Deployers.sol";
 import {Test} from "forge-std/Test.sol";
 import {Airlock, ModuleState, SetModuleState} from "src/Airlock.sol";
+import {Ownable} from "@openzeppelin/access/Ownable.sol";
 import {TokenFactory} from "src/TokenFactory.sol";
 import {DopplerFactory} from "src/DopplerFactory.sol";
 import {GovernanceFactory} from "src/GovernanceFactory.sol";
@@ -37,6 +38,12 @@ contract AirlockTest is Test, Deployers {
     function test_setModuleState_EmitsEvent() public {
         vm.expectEmit();
         emit SetModuleState(address(0xbeef), ModuleState.TokenFactory);
+        airlock.setModuleState(address(0xbeef), ModuleState.TokenFactory);
+    }
+
+    function test_setModuleState_RevertsWhenSenderNotOwner() public {
+        vm.expectRevert(abi.encodeWithSelector(Ownable.OwnableUnauthorizedAccount.selector, address(0xb0b)));
+        vm.prank(address(0xb0b));
         airlock.setModuleState(address(0xbeef), ModuleState.TokenFactory);
     }
 }
