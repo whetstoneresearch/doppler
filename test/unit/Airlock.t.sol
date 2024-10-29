7010d5f7b4a4cc6d7c5d01c72f900edefa813f80
clemlak <clementlakhal@gmail.com>
2024-10-29 14:35:22 +0400

    test: update Airlock test setup

--- test/unit/Airlock.t.sol
+++ test/unit/Airlock.t.sol
@@ -1,14 +1,36 @@
 /// SPDX-License-Identifier: UNLICENSED
 pragma solidity ^0.8.13;
 
-import {BaseTest} from "test/shared/BaseTest.sol";
-import {Airlock} from "src/Airlock.sol";
+import {Deployers} from "v4-core/test/utils/Deployers.sol";
+import {Test} from "forge-std/Test.sol";
+import {Airlock, ModuleState} from "src/Airlock.sol";
+import {TokenFactory} from "src/TokenFactory.sol";
+import {DopplerFactory} from "src/DopplerFactory.sol";
+import {GovernanceFactory} from "src/GovernanceFactory.sol";
+import {UniswapV2Migrator} from "src/UniswapV2Migrator.sol";
 
-contract AirlockTest is BaseTest {
-    Airlock public airlock;
+contract AirlockTest is Test, Deployers {
+    Airlock airlock;
+    TokenFactory tokenFactory;
+    DopplerFactory dopplerFactory;
+    GovernanceFactory governanceFactory;
+    UniswapV2Migrator migrator;
 
-    function setUp() public override {
-        this.setUp();
+    function setUp() public {
+        deployFreshManager();
         airlock = new Airlock(manager);
+        tokenFactory = new TokenFactory();
+        dopplerFactory = new DopplerFactory();
+        governanceFactory = new GovernanceFactory();
+
+        airlock.setModuleState(address(tokenFactory), ModuleState.TokenFactory);
+        airlock.setModuleState(address(dopplerFactory), ModuleState.HookFactory);
+        airlock.setModuleState(address(governanceFactory), ModuleState.GovernanceFactory);
+        airlock.setModuleState(address(migrator), ModuleState.Migrator);
+    }
+
+    function test_setModuleState_SetsState() public {
+        airlock.setModuleState(address(0xbeef), ModuleState.TokenFactory);
+        assertEq(uint8(airlock.getModuleState(address(0xbeef))), uint8(ModuleState.TokenFactory));
     }
 }
