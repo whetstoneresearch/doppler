7f85209fa8c41182c7e8d25bc2012f5d123f0073
clemlak <clementlakhal@gmail.com>
2024-10-29 11:41:26 +0400

    chore: add NatSpec to ITokenFactory

--- src/interfaces/ITokenFactory.sol
+++ src/interfaces/ITokenFactory.sol
@@ -1,7 +1,21 @@
 // SPDX-License-Identifier: UNLICENSED
 pragma solidity ^0.8.13;
 
+/**
+ * @title Token Factory Interface
+ * @notice Contracts deploying new asset token must implement this interface.
+ */
 interface ITokenFactory {
+    /**
+     * @notice Deploys a new asset token.
+     * @param name Name of the token
+     * @param symbol Symbol of the token
+     * @param initialSupply Initial supply that will be minted
+     * @param recipient Address receiving the initial supply
+     * @param owner Address receiving the ownership of the token
+     * @param tokenData Extra data to be used by the factory
+     * @return Address of the newly deployed token
+     */
     function create(
         string memory name,
         string memory symbol,
