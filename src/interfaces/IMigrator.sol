8162f9be51958b415b378b734b49113d067fff71
clemlak <clementlakhal@gmail.com>
2024-10-29 13:59:12 +0400

    feat: add liquidity returned parameter to migrate function

--- src/interfaces/IMigrator.sol
+++ src/interfaces/IMigrator.sol
@@ -5,8 +5,12 @@ pragma solidity ^0.8.13;
  * @notice Generic interface to migrate current liquidity to a new pool
  */
 interface IMigrator {
-    function migrate(address asset, address numeraire, uint256 amountAsset, uint256 amountNumeraire, bytes memory data)
-        external
-        payable
-        returns (address pool);
+    function migrate(
+        address asset,
+        address numeraire,
+        uint256 amountAsset,
+        uint256 amountNumeraire,
+        address recipient,
+        bytes memory data
+    ) external payable returns (address pool, uint256 liquidity);
 }
