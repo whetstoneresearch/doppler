6098e67b200c0e8549891e492e9c0423499b4b84
clemlak <clementlakhal@gmail.com>
2024-10-30 15:58:48 +0400

    feat: clean up create function parameters in DopplerFactory

--- src/DopplerFactory.sol
+++ src/DopplerFactory.sol
@@ -7,24 +7,22 @@ import {IHookFactory} from "src/interfaces/IHookFactory.sol";
 import {Doppler} from "src/Doppler.sol";
 
 contract DopplerFactory is IHookFactory {
-    function create(
-        IPoolManager poolManager,
-        uint256 numTokensToSell,
-        bool isToken0,
-        int24 startingTick,
-        int24 endingTick,
-        bytes memory data,
-        bytes32 salt
-    ) external returns (address) {
+    function create(IPoolManager poolManager, uint256 numTokensToSell, bytes memory data, bytes32 salt)
+        external
+        returns (address)
+    {
         (
             uint256 minimumProceeds,
             uint256 maximumProceeds,
             uint256 startingTime,
             uint256 endingTime,
+            int24 startingTick,
+            int24 endingTick,
             uint256 epochLength,
             int24 gamma,
+            bool isToken0,
             uint256 numPDSlugs
-        ) = abi.decode(data, (uint256, uint256, uint256, uint256, uint256, int24, uint256));
+        ) = abi.decode(data, (uint256, uint256, uint256, uint256, int24, int24, uint256, int24, bool, uint256));
 
         return address(
             new Doppler{salt: salt}(
