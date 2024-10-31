96526b2f249015d93c0cf22601aea4b0e13f3aae
clemlak <clementlakhal@gmail.com>
2024-10-31 16:09:38 +0400

    test: add test_create_RevertsIfWrongInitialSupply

--- test/unit/Airlock.t.sol
+++ test/unit/Airlock.t.sol
@@ -9,7 +9,7 @@ import {PoolKey} from "v4-core/src/types/PoolKey.sol";
 import {IHooks} from "v4-core/src/interfaces/IHooks.sol";
 import {Currency} from "v4-core/src/types/Currency.sol";
 
-import {Airlock, ModuleState, WrongModuleState, SetModuleState} from "src/Airlock.sol";
+import {Airlock, ModuleState, WrongModuleState, SetModuleState, WrongInitialSupply} from "src/Airlock.sol";
 import {TokenFactory} from "src/TokenFactory.sol";
 import {DopplerFactory} from "src/DopplerFactory.sol";
 import {GovernanceFactory} from "src/GovernanceFactory.sol";
@@ -165,5 +165,154 @@ contract AirlockTest is Test, Deployers {
         airlock.setModuleState(address(migrator), ModuleState.NotWhitelisted);
         vm.expectRevert(WrongModuleState.selector);
         _create();
+    }
+
+    function test_create_RevertsIfWrongInitialSupply() public {
+        {
+            // Trying to mint more tokens than the amount to sell.
+            uint256 initialSupply = DEFAULT_INITIAL_SUPPLY + 1;
+            uint256 numToSell = DEFAULT_INITIAL_SUPPLY;
+
+            (bytes32 salt, address hook, address token) = mine(
+                address(tokenFactory),
+                address(dopplerFactory),
+                MineParams({
+                    poolManager: address(manager),
+                    numTokensToSell: numToSell,
+                    minTick: DEFAULT_START_TICK,
+                    maxTick: DEFAULT_END_TICK,
+                    airlock: address(airlock),
+                    name: DEFAULT_TOKEN_NAME,
+                    symbol: DEFAULT_TOKEN_SYMBOL,
+                    initialSupply: initialSupply,
+                    numeraire: address(0),
+                    startingTime: DEFAULT_STARTING_TIME,
+                    endingTime: DEFAULT_ENDING_TIME,
+                    minimumProceeds: DEFAULT_MIN_PROCEEDS,
+                    maximumProceeds: DEFAULT_MAX_PROCEEDS,
+                    epochLength: DEFAULT_EPOCH_LENGTH,
+                    gamma: DEFAULT_GAMMA,
+                    numPDSlugs: DEFAULT_PD_SLUGS
+                })
+            );
+
+            PoolKey memory poolKey = PoolKey({
+                currency0: Currency.wrap(address(0)),
+                currency1: Currency.wrap(token),
+                fee: DEFAULT_FEE,
+                tickSpacing: DEFAULT_TICK_SPACING,
+                hooks: IHooks(hook)
+            });
+
+            bytes memory hookFactoryData = abi.encode(
+                DEFAULT_MIN_PROCEEDS,
+                DEFAULT_MAX_PROCEEDS,
+                DEFAULT_STARTING_TIME,
+                DEFAULT_ENDING_TIME,
+                DEFAULT_START_TICK,
+                DEFAULT_END_TICK,
+                DEFAULT_EPOCH_LENGTH,
+                DEFAULT_GAMMA,
+                false,
+                DEFAULT_PD_SLUGS
+            );
+
+            vm.expectRevert(WrongInitialSupply.selector);
+            airlock.create(
+                DEFAULT_TOKEN_NAME,
+                DEFAULT_TOKEN_SYMBOL,
+                initialSupply,
+                numToSell,
+                poolKey,
+                DEFAULT_OWNER,
+                new address[](0),
+                new uint256[](0),
+                tokenFactory,
+                new bytes(0),
+                governanceFactory,
+                new bytes(0),
+                dopplerFactory,
+                hookFactoryData,
+                migrator,
+                salt
+            );
+        }
+
+        {
+            // Trying to allocate too many tokens to the team.
+            uint256 initialSupply = DEFAULT_INITIAL_SUPPLY;
+            uint256 numToSell = DEFAULT_INITIAL_SUPPLY;
+
+            uint256[] memory amounts = new uint256[](4);
+            address[] memory recipients = new address[](4);
+
+            for (uint256 i; i < amounts.length; i++) {
+                amounts[i] = 10_000 ether;
+            }
+
+            (bytes32 salt, address hook, address token) = mine(
+                address(tokenFactory),
+                address(dopplerFactory),
+                MineParams({
+                    poolManager: address(manager),
+                    numTokensToSell: numToSell,
+                    minTick: DEFAULT_START_TICK,
+                    maxTick: DEFAULT_END_TICK,
+                    airlock: address(airlock),
+                    name: DEFAULT_TOKEN_NAME,
+                    symbol: DEFAULT_TOKEN_SYMBOL,
+                    initialSupply: initialSupply,
+                    numeraire: address(0),
+                    startingTime: DEFAULT_STARTING_TIME,
+                    endingTime: DEFAULT_ENDING_TIME,
+                    minimumProceeds: DEFAULT_MIN_PROCEEDS,
+                    maximumProceeds: DEFAULT_MAX_PROCEEDS,
+                    epochLength: DEFAULT_EPOCH_LENGTH,
+                    gamma: DEFAULT_GAMMA,
+                    numPDSlugs: DEFAULT_PD_SLUGS
+                })
+            );
+
+            PoolKey memory poolKey = PoolKey({
+                currency0: Currency.wrap(address(0)),
+                currency1: Currency.wrap(token),
+                fee: DEFAULT_FEE,
+                tickSpacing: DEFAULT_TICK_SPACING,
+                hooks: IHooks(hook)
+            });
+
+            bytes memory hookFactoryData = abi.encode(
+                DEFAULT_MIN_PROCEEDS,
+                DEFAULT_MAX_PROCEEDS,
+                DEFAULT_STARTING_TIME,
+                DEFAULT_ENDING_TIME,
+                DEFAULT_START_TICK,
+                DEFAULT_END_TICK,
+                DEFAULT_EPOCH_LENGTH,
+                DEFAULT_GAMMA,
+                false,
+                DEFAULT_PD_SLUGS
+            );
+
+            vm.expectRevert(WrongInitialSupply.selector);
+            airlock.create(
+                DEFAULT_TOKEN_NAME,
+                DEFAULT_TOKEN_SYMBOL,
+                initialSupply,
+                numToSell,
+                poolKey,
+                DEFAULT_OWNER,
+                recipients,
+                amounts,
+                tokenFactory,
+                new bytes(0),
+                governanceFactory,
+                new bytes(0),
+                dopplerFactory,
+                hookFactoryData,
+                migrator,
+                salt
+            );
+        }
     }
 }
