f627fd89171eee0703c6913b3f1352ee4193c3c0
clemlak <clementlakhal@gmail.com>
2024-10-31 15:22:01 +0400

    test: add test_create_RevertsIfWrongHookFactory

--- test/unit/Airlock.t.sol
+++ test/unit/Airlock.t.sol
@@ -154,4 +154,10 @@ contract AirlockTest is Test, Deployers {
         vm.expectRevert(WrongModuleState.selector);
         _create();
     }
+
+    function test_create_RevertsIfWrongHookFactory() public {
+        airlock.setModuleState(address(dopplerFactory), ModuleState.NotWhitelisted);
+        vm.expectRevert(WrongModuleState.selector);
+        _create();
+    }
 }
