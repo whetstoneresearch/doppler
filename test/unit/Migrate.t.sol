1408e83fd590c04bfccf41d2ae64443bb062372c
clemlak <clementlakhal@gmail.com>
2024-11-05 15:22:35 +0400

    test: add test_migrate_RevertsIfConditionsNotMet

--- test/unit/Migrate.t.sol
+++ test/unit/Migrate.t.sol
@@ -1,11 +1,17 @@
 pragma solidity 0.8.26;
 
 import { BaseTest } from "test/shared/BaseTest.sol";
-import { SenderNotAirlock } from "src/Doppler.sol";
+import { SenderNotAirlock, CannotMigrate } from "src/Doppler.sol";
 
 contract MigrateTest is BaseTest {
     function test_migrate_RevertsIfSenderNotAirlock() public {
         vm.expectRevert(SenderNotAirlock.selector);
+        hook.migrate();
+    }
+
+    function test_migrate_RevertsIfConditionsNotMet() public {
+        vm.startPrank(hook.airlock());
+        vm.expectRevert(CannotMigrate.selector);
         hook.migrate();
     }
 }
