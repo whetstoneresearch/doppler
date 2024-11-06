6019814b535782b6683022e738b7f6458e3d8aee
clemlak <clementlakhal@gmail.com>
2024-11-06 15:54:24 +0400

    test: add test_migrate_NoMoreFundsInHook

--- test/unit/Migrate.t.sol
+++ test/unit/Migrate.t.sol
@@ -1,8 +1,10 @@
 pragma solidity 0.8.26;
 
-import { BaseTest } from "test/shared/BaseTest.sol";
-import { SenderNotAirlock, CannotMigrate } from "src/Doppler.sol";
+import { console } from "forge-std/console.sol";
 import { StateLibrary, IPoolManager, PoolId } from "v4-core/src/libraries/StateLibrary.sol";
+import { SenderNotAirlock, CannotMigrate } from "src/Doppler.sol";
+import { ERC20 } from "solmate/src/tokens/ERC20.sol";
+import { BaseTest } from "test/shared/BaseTest.sol";
 
 contract MigrateTest is BaseTest {
     using StateLibrary for IPoolManager;
@@ -35,5 +37,42 @@ contract MigrateTest is BaseTest {
             );
             assertEq(liquidity, 0, "liquidity should be 0");
         }
+    }
+
+    function test_migrate_NoMoreFundsInHook() public {
+        console.log("In Pool %e", ERC20(isToken0 ? token0 : token1).balanceOf(address(manager)));
+
+        uint256 preBalance = ERC20(isToken0 ? token0 : token1).balanceOf(address(hook));
+        console.log("preBalance %e", preBalance);
+
+        vm.warp(hook.getStartingTime());
+
+        _debugPositions("Initial positions");
+
+        buyExactOut(1 ether);
+        console.log("In Pool %e", ERC20(isToken0 ? token0 : token1).balanceOf(address(manager)));
+
+        _debugPositions("After swap");
+
+        buyExactOut(hook.getNumTokensToSell() - 1);
+
+        _debugPositions("After swap 2");
+
+        uint256 midBalance = ERC20(isToken0 ? token0 : token1).balanceOf(address(hook));
+        console.log("midBalance %e", midBalance);
+
+        vm.warp(hook.getEndingTime());
+        vm.prank(hook.airlock());
+        hook.migrate();
+
+        _debugPositions("After migration");
+
+        if (usingEth) {
+            assertEq(address(hook).balance, 0, "hook should have no ETH");
+        } else {
+            assertEq(ERC20(token0).balanceOf(address(hook)), 0, "hook should have no token0");
+        }
+
+        assertEq(ERC20(token1).balanceOf(address(hook)), 0, "hook should have no token1");
     }
 }
