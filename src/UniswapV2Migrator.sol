ec1f9e5a7c54ba45f8032b9d5d31317ddd5fd625
clemlak <clementlakhal@gmail.com>
2024-11-20 17:59:37 +0400

    feat: add createPool and refactor migrate function

--- src/UniswapV2Migrator.sol
+++ src/UniswapV2Migrator.sol
@@ -3,6 +3,7 @@ pragma solidity ^0.8.13;
 
 import { IMigrator } from "src/interfaces/IMigrator.sol";
 import { SafeTransferLib, ERC20 } from "solmate/src/utils/SafeTransferLib.sol";
+import { FixedPointMathLib } from "solmate/src/utils/FixedPointMathLib.sol";
 
 interface IUniswapV2Router02 {
     function WETH() external pure returns (address);
@@ -28,6 +29,10 @@ interface IUniswapV2Router02 {
     ) external payable returns (uint256 amountToken, uint256 amountETH, uint256 liquidity);
 }
 
+interface IUniswapV2Pair {
+    function getReserves() external view returns (uint112 _reserve0, uint112 _reserve1, uint32 _blockTimestampLast);
+}
+
 interface IUniswapV2Factory {
     function createPair(address tokenA, address tokenB) external returns (address pair);
     function getPair(address tokenA, address tokenB) external view returns (address pair);
@@ -38,8 +43,12 @@ interface IUniswapV2Factory {
  * @notice Takes care of migrating liquidity into a Uniswap V2 pool
  */
 contract UniswapV2Migrator is IMigrator {
+    using FixedPointMathLib for uint256;
+
     IUniswapV2Factory public immutable factory;
     IUniswapV2Router02 public immutable router;
+
+    mapping(address token0 => mapping(address token1 => address pool)) public getPool;
 
     /**
      * @param factory_ Address of the Uniswap V2 factory
@@ -50,12 +59,24 @@ contract UniswapV2Migrator is IMigrator {
         router = router_;
     }
 
+    function createPool(address token0, address token1) external returns (address) {
+        if (token0 == address(0)) token0 = router.WETH();
+
+        address pool = factory.getPair(token0, token1);
+
+        if (pool == address(0)) {
+            pool = factory.createPair(token0, token1);
+        }
+
+        getPool[token0][token1] = pool;
+
+        return pool;
+    }
+
     /**
      * @notice Migrates the liquidity into a Uniswap V2 pool
      * @param token0 Smaller address of the two tokens
      * @param token1 Larger address of the two tokens
-     * @param amount0 Amount of token0 to be migrated
-     * @param amount1 Amount of token1 to be migrated
      * @param recipient Address receiving the liquidity pool tokens
      * @return pool Address of the Uniswap V2 pool
      * @return liquidity Amount of liquidity tokens minted
@@ -63,29 +84,42 @@ contract UniswapV2Migrator is IMigrator {
     function migrate(
         address token0,
         address token1,
-        uint256 amount0,
-        uint256 amount1,
+        uint256 price,
         address recipient,
         bytes memory
     ) external payable returns (address pool, uint256 liquidity) {
-        pool = factory.getPair(token0, token1);
+        uint256 balance0 = token0 == address(0) ? address(this).balance : ERC20(token0).balanceOf(address(this));
+        uint256 balance1 = ERC20(token1).balanceOf(address(this));
 
-        if (pool == address(0)) {
-            pool = factory.createPair(token0 == address(0) ? router.WETH() : token0, token1);
+        pool = getPool[token0 == address(0) ? router.WETH() : token0][token1];
+
+        uint256 amount0 = price.mulWadDown(balance1);
+        uint256 amount1 = balance0.divWadDown(price);
+
+        if (amount0 > balance0) {
+            amount0 = balance0;
+            amount1 = amount0.divWadDown(price);
+        } else if (amount1 > balance1) {
+            amount1 = balance1;
+            amount0 = price.mulWadDown(amount1);
         }
 
-        if (token0 != address(0)) ERC20(token0).approve(address(router), amount0);
         ERC20(token1).approve(address(router), amount1);
 
         if (token0 == address(0)) {
-            (,, liquidity) = router.addLiquidityETH{ value: amount0 }(token1, amount0, 0, 0, recipient, block.timestamp);
+            (,, liquidity) = router.addLiquidityETH{ value: amount0 }(token1, amount1, 0, 0, recipient, block.timestamp);
             SafeTransferLib.safeTransferETH(recipient, address(this).balance);
+            if (address(this).balance > 0) SafeTransferLib.safeTransferETH(recipient, address(this).balance);
         } else {
             ERC20(token0).approve(address(router), amount0);
-            (,, liquidity) = router.addLiquidity(token0, token1, 0, 0, 0, 0, msg.sender, block.timestamp);
-            SafeTransferLib.safeTransfer(ERC20(token0), recipient, ERC20(token0).balanceOf(address(this)));
+            (,, liquidity) = router.addLiquidity(token0, token1, amount0, amount1, 0, 0, msg.sender, block.timestamp);
+            ERC20(token0).approve(address(router), 0);
+            uint256 dust0 = ERC20(token0).balanceOf(address(this));
+            if (dust0 > 0) SafeTransferLib.safeTransfer(ERC20(token0), recipient, dust0);
         }
 
-        SafeTransferLib.safeTransfer(ERC20(token1), recipient, ERC20(token1).balanceOf(address(this)));
+        ERC20(token1).approve(address(router), 0);
+        uint256 dust1 = ERC20(token1).balanceOf(address(this));
+        if (dust1 > 0) SafeTransferLib.safeTransfer(ERC20(token1), recipient, dust1);
     }
 }
