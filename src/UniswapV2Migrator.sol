f6e2cb796ad1203e388369c981353bb552467c8c
clemlak <clementlakhal@gmail.com>
2024-11-22 16:10:14 +0400

    feat: clean up Uniswap V2 interfaces

--- src/UniswapV2Migrator.sol
+++ src/UniswapV2Migrator.sol
@@ -9,30 +9,9 @@ import { WETH as IWETH } from "solmate/src/tokens/WETH.sol";
 
 interface IUniswapV2Router02 {
     function WETH() external pure returns (address);
-
-    function addLiquidity(
-        address tokenA,
-        address tokenB,
-        uint256 amountADesired,
-        uint256 amountBDesired,
-        uint256 amountAMin,
-        uint256 amountBMin,
-        address to,
-        uint256 deadline
-    ) external returns (uint256 amountA, uint256 amountB, uint256 liquidity);
-
-    function addLiquidityETH(
-        address token,
-        uint256 amountTokenDesired,
-        uint256 amountTokenMin,
-        uint256 amountETHMin,
-        address to,
-        uint256 deadline
-    ) external payable returns (uint256 amountToken, uint256 amountETH, uint256 liquidity);
 }
 
 interface IUniswapV2Pair {
-    function getReserves() external view returns (uint112 _reserve0, uint112 _reserve1, uint32 _blockTimestampLast);
     function mint(
         address to
     ) external returns (uint256 liquidity);
