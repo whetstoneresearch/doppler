055cb3cca773e4e295dd1c3075440b422b7f799f
clemlak <clementlakhal@gmail.com>
2024-11-26 17:45:49 +0400

    fix: refactor migration tokens transfer

--- src/UniswapV2Migrator.sol
+++ src/UniswapV2Migrator.sol
@@ -110,14 +110,12 @@ contract UniswapV2Migrator is IMigrator {
 
         if (token0 == address(weth)) {
             weth.deposit{ value: amount0 }();
-            ERC20(token1).safeTransfer(pool, amount1);
         } else if (token1 == address(weth)) {
-            ERC20(token0).transfer(pool, amount0);
             weth.deposit{ value: amount1 }();
-        } else {
-            ERC20(token0).safeTransfer(pool, amount0);
-            ERC20(token1).safeTransfer(pool, amount1);
         }
+
+        ERC20(token0).safeTransfer(pool, amount0);
+        ERC20(token1).safeTransfer(pool, amount1);
 
         liquidity = IUniswapV2Pair(pool).mint(recipient);
 
