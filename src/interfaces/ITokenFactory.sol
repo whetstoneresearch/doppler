1d31051c9215562650c2072fffcaa265e3eb5209
kinrezc <matt.czernik@gmail.com>
2024-12-05 16:21:22 -0500

    fix: add missing natspec

--- src/interfaces/ITokenFactory.sol
+++ src/interfaces/ITokenFactory.sol
@@ -13,7 +13,9 @@ interface ITokenFactory {
      * @param initialSupply Initial supply that will be minted
      * @param recipient Address receiving the initial supply
      * @param owner Address receiving the ownership of the token
+     * @param pool Address of the pool contract
      * @param tokenData Extra data to be used by the factory
+     * @param salt Salt used in create2 deployment to determine contract address
      * @return Address of the newly deployed token
      */
     function create(
