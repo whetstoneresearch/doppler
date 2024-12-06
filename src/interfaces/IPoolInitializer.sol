690637f923197826df22b0bef6384ca5c2e5f0c6
clemlak <clementlakhal@gmail.com>
2024-12-06 16:16:12 +0400

    feat: rename HookFactory to PoolInitializer

---
+++ src/interfaces/IPoolInitializer.sol
@@ -0,0 +1,25 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity ^0.8.13;
+
+interface IPoolInitializer {
+    /**
+     * @notice Creates a new pool to bootstrap liquidity
+     * @param numTokensToSell Amount of asset tokens to sell
+     * @param salt Salt for the create2 deployment
+     * @param data Arbitrary data to pass
+     * @param pool Address of the pool
+     */
+    function initialize(uint256 numTokensToSell, bytes32 salt, bytes memory data) external returns (address pool);
+
+    function exitLiquidity() external;
+}
+
+interface IHook {
+    /**
+     * @notice Triggers the migration stage of the hook contract
+     * @return Price of the pool
+     */
+    function migrate(
+        address recipient
+    ) external returns (uint256);
+}
