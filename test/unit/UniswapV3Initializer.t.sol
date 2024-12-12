9d131bea474bc294eab4fcf989260b3de32406f5
clemlak <clementlakhal@gmail.com>
2024-12-12 11:36:44 +0400

    test: use address constants instead of raw addresses

--- test/unit/UniswapV3Initializer.t.sol
+++ test/unit/UniswapV3Initializer.t.sol
@@ -7,18 +7,19 @@ import { IUniswapV3Factory } from "@v3-core/interfaces/IUniswapV3Factory.sol";
 import { UniswapV3Initializer, OnlyAirlock, PoolAlreadyInitialized } from "src/UniswapV3Initializer.sol";
 import { DERC20 } from "src/DERC20.sol";
 
+import { WETH_MAINNET, UNISWAP_V3_FACTORY_MAINNET } from "test/shared/Addresses.sol";
+
 contract UniswapV3InitializerTest is Test {
     UniswapV3Initializer public initializer;
 
     function setUp() public {
         vm.createSelectFork(vm.envString("MAINNET_RPC_URL"), 21_093_509);
-        initializer =
-            new UniswapV3Initializer(address(this), IUniswapV3Factory(0x1F98431c8aD98523631AE4a59f267346ea31F984));
+        initializer = new UniswapV3Initializer(address(this), IUniswapV3Factory(UNISWAP_V3_FACTORY_MAINNET));
     }
 
     function test_constructor() public view {
         assertEq(address(initializer.airlock()), address(this), "Wrong airlock");
-        assertEq(address(initializer.factory()), address(0x1F98431c8aD98523631AE4a59f267346ea31F984), "Wrong factory");
+        assertEq(address(initializer.factory()), address(UNISWAP_V3_FACTORY_MAINNET), "Wrong factory");
     }
 
     function test_initialize() public {
@@ -27,7 +28,7 @@ contract UniswapV3InitializerTest is Test {
 
         address pool = initializer.initialize(
             address(token),
-            address(0xC02aaA39b223FE8D0A0e5C4F27eAD9083C756Cc2),
+            address(WETH_MAINNET),
             1e27,
             bytes32(0),
             abi.encode(uint24(3000), int24(-200_040), int24(-167_520))
@@ -51,7 +52,7 @@ contract UniswapV3InitializerTest is Test {
 
         initializer.initialize(
             address(token),
-            address(0xC02aaA39b223FE8D0A0e5C4F27eAD9083C756Cc2),
+            address(WETH_MAINNET),
             1e27,
             bytes32(0),
             abi.encode(uint24(3000), int24(-200_040), int24(-167_520))
@@ -60,7 +61,7 @@ contract UniswapV3InitializerTest is Test {
         vm.expectRevert(PoolAlreadyInitialized.selector);
         initializer.initialize(
             address(token),
-            address(0xC02aaA39b223FE8D0A0e5C4F27eAD9083C756Cc2),
+            address(WETH_MAINNET),
             1e27,
             bytes32(0),
             abi.encode(uint24(3000), int24(-200_040), int24(-167_520))
@@ -79,7 +80,7 @@ contract UniswapV3InitializerTest is Test {
 
         address pool = initializer.initialize(
             address(token),
-            address(0xC02aaA39b223FE8D0A0e5C4F27eAD9083C756Cc2),
+            address(WETH_MAINNET),
             1e27,
             bytes32(0),
             abi.encode(uint24(3000), int24(-200_040), int24(-167_520))
