0aa4236cfa6ae47f503c3886d8f9040f6ec1338a
clemlak <clementlakhal@gmail.com>
2024-12-16 15:14:47 +0400

    feat: return sqrtPriceX96 from exitLiquidity

--- src/interfaces/IPoolInitializer.sol
+++ src/interfaces/IPoolInitializer.sol
@@ -19,7 +19,7 @@ interface IPoolInitializer {
 
     function exitLiquidity(
         address asset
-    ) external returns (address token0, uint256 amount0, address token1, uint256 amount1);
+    ) external returns (uint160 sqrtPriceX96, address token0, uint256 amount0, address token1, uint256 amount1);
 }
 
 interface IHook {
