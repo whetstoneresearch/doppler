1678fedb112eff3e67bdbfbad6e358b72a92de6f
clemlak <clementlakhal@gmail.com>
2025-01-05 21:42:26 +0400

    test: add tests for MaxPreMintPerAddressExceeded and MaxTotalPreMintExceeded

--- test/unit/DERC20.t.sol
+++ test/unit/DERC20.t.sol
@@ -2,7 +2,14 @@
 pragma solidity ^0.8.13;
 
 import { Test } from "forge-std/Test.sol";
-import { DERC20, ArrayLengthsMismatch } from "src/DERC20.sol";
+import {
+    DERC20,
+    ArrayLengthsMismatch,
+    MaxPreMintPerAddressExceeded,
+    MaxTotalPreMintExceeded,
+    MAX_PRE_MINT_PER_ADDRESS_WAD,
+    MAX_TOTAL_PRE_MINT_WAD
+} from "src/DERC20.sol";
 
 uint256 constant INITIAL_SUPPLY = 1e26;
 uint256 constant YEARLY_MINT_CAP = 1e25;
@@ -49,7 +56,60 @@ contract DERC20Test is Test {
         amounts[0] = 1e23;
 
         vm.expectRevert(ArrayLengthsMismatch.selector);
+        token = new DERC20(
+            NAME, SYMBOL, INITIAL_SUPPLY, RECIPIENT, OWNER, YEARLY_MINT_CAP, VESTING_DURATION, recipients, amounts
+        );
+    }
 
+    function test_constructor_RevertsWhenMaxPreMintPerAddressExceeded() public {
+        address[] memory recipients = new address[](1);
+        recipients[0] = address(0xa);
+
+        uint256[] memory amounts = new uint256[](1);
+        amounts[0] = INITIAL_SUPPLY * MAX_PRE_MINT_PER_ADDRESS_WAD / 1e18 + 1;
+
+        vm.expectRevert(
+            abi.encodeWithSelector(
+                MaxPreMintPerAddressExceeded.selector, amounts[0], INITIAL_SUPPLY * MAX_PRE_MINT_PER_ADDRESS_WAD / 1e18
+            )
+        );
+        token = new DERC20(
+            NAME, SYMBOL, INITIAL_SUPPLY, RECIPIENT, OWNER, YEARLY_MINT_CAP, VESTING_DURATION, recipients, amounts
+        );
+    }
+
+    function test_constructor_RevertsWhenMaxPreMintPerAddressExceededReusingAddress() public {
+        address[] memory recipients = new address[](2);
+        recipients[0] = address(0xa);
+        recipients[1] = address(0xa);
+
+        uint256[] memory amounts = new uint256[](2);
+        amounts[0] = INITIAL_SUPPLY * MAX_PRE_MINT_PER_ADDRESS_WAD / 1e18;
+        amounts[1] = INITIAL_SUPPLY * MAX_PRE_MINT_PER_ADDRESS_WAD / 1e18;
+
+        vm.expectRevert(abi.encodeWithSelector(MaxPreMintPerAddressExceeded.selector, amounts[0] * 2, amounts[0]));
+        token = new DERC20(
+            NAME, SYMBOL, INITIAL_SUPPLY, RECIPIENT, OWNER, YEARLY_MINT_CAP, VESTING_DURATION, recipients, amounts
+        );
+    }
+
+    function test_constructor_RevertsWhenMaxTotalPreMintExceeded() public {
+        uint256 maxTotalPreMint = INITIAL_SUPPLY * MAX_TOTAL_PRE_MINT_WAD / 1 ether;
+        uint256 length = MAX_TOTAL_PRE_MINT_WAD / MAX_PRE_MINT_PER_ADDRESS_WAD + 1;
+
+        address[] memory recipients = new address[](length);
+        uint256[] memory amounts = new uint256[](length);
+
+        for (uint256 i; i != length; ++i) {
+            recipients[i] = address(uint160(i));
+            amounts[i] = INITIAL_SUPPLY * MAX_PRE_MINT_PER_ADDRESS_WAD / 1e18;
+        }
+
+        vm.expectRevert(
+            abi.encodeWithSelector(
+                MaxTotalPreMintExceeded.selector, maxTotalPreMint * 1.1 ether / 1e18, maxTotalPreMint
+            )
+        );
         token = new DERC20(
             NAME, SYMBOL, INITIAL_SUPPLY, RECIPIENT, OWNER, YEARLY_MINT_CAP, VESTING_DURATION, recipients, amounts
         );
