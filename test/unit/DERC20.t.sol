35f6869d63a58b55aa33cfec4eb1d66d34d0bfd0
clemlak <clementlakhal@gmail.com>
2025-01-05 15:51:25 +0400

    test: add new constants to DERC20 test

--- test/unit/DERC20.t.sol
+++ test/unit/DERC20.t.sol
@@ -7,6 +7,10 @@ import { DERC20 } from "src/DERC20.sol";
 uint256 constant INITIAL_SUPPLY = 1e26;
 uint256 constant YEARLY_MINT_CAP = 1e25;
 uint256 constant VESTING_DURATION = 365 days;
+string constant NAME = "Test";
+string constant SYMBOL = "TST";
+address constant RECIPIENT = address(0xa71ce);
+address constant OWNER = address(0xb0b);
 
 contract DERC20Test is Test {
     DERC20 public token;
@@ -21,25 +25,15 @@ contract DERC20Test is Test {
         amounts[1] = 2e23;
 
         token = new DERC20(
-            "Test",
-            "TST",
-            INITIAL_SUPPLY,
-            address(0xb0b),
-            address(0xbeef),
-            YEARLY_MINT_CAP,
-            VESTING_DURATION,
-            recipients,
-            amounts
+            NAME, SYMBOL, INITIAL_SUPPLY, RECIPIENT, OWNER, YEARLY_MINT_CAP, VESTING_DURATION, recipients, amounts
         );
 
-        assertEq(token.name(), "Test", "Wrong name");
-        assertEq(token.symbol(), "TST", "Wrong symbol");
+        assertEq(token.name(), NAME, "Wrong name");
+        assertEq(token.symbol(), SYMBOL, "Wrong symbol");
         assertEq(token.totalSupply(), INITIAL_SUPPLY, "Wrong total supply");
-        assertEq(
-            token.balanceOf(address(0xb0b)), INITIAL_SUPPLY - amounts[0] - amounts[1], "Wrong balance of recipient"
-        );
+        assertEq(token.balanceOf(RECIPIENT), INITIAL_SUPPLY - amounts[0] - amounts[1], "Wrong balance of recipient");
         assertEq(token.mintStartDate(), block.timestamp + 365 days, "Wrong mint start date");
-        assertEq(token.owner(), address(0xbeef), "Wrong owner");
+        assertEq(token.owner(), OWNER, "Wrong owner");
         assertEq(token.yearlyMintCap(), YEARLY_MINT_CAP, "Wrong yearly mint cap");
         assertEq(token.vestingStart(), block.timestamp, "Wrong vesting start");
         assertEq(token.vestingDuration(), VESTING_DURATION, "Wrong vesting duration");
