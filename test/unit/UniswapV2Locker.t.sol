c38712502258c9a161affe512aa3d1eb84765265
clemlak <clementlakhal@gmail.com>
2025-01-08 19:01:17 +0400

    test: add test_receiveAndLock_RevertsWhenNoBalanceToLock

--- test/unit/UniswapV2Locker.t.sol
+++ test/unit/UniswapV2Locker.t.sol
@@ -2,11 +2,12 @@
 pragma solidity ^0.8.13;
 
 import { Test } from "forge-std/Test.sol";
-import { UniswapV2Locker } from "src/UniswapV2Locker.sol";
+import { UniswapV2Locker, NoBalanceToLock } from "src/UniswapV2Locker.sol";
 import { UNISWAP_V2_FACTORY_MAINNET } from "test/shared/Addresses.sol";
 import { UniswapV2Migrator } from "src/UniswapV2Migrator.sol";
 import { Airlock } from "src/Airlock.sol";
 import { IUniswapV2Factory } from "src/interfaces/IUniswapV2Factory.sol";
+import { IUniswapV2Pair } from "src/interfaces/IUniswapV2Pair.sol";
 
 contract UniswapV2LockerTest is Test {
     UniswapV2Locker public locker;
@@ -16,5 +17,12 @@ contract UniswapV2LockerTest is Test {
         locker = new UniswapV2Locker(
             Airlock(payable(address(this))), IUniswapV2Factory(UNISWAP_V2_FACTORY_MAINNET), migrator
         );
+    }
+
+    function test_receiveAndLock_RevertsWhenNoBalanceToLock() public {
+        address pool = address(0x123);
+        vm.mockCall(pool, abi.encodeWithSelector(IUniswapV2Pair.balanceOf.selector, address(locker)), abi.encode(0));
+        vm.expectRevert(NoBalanceToLock.selector);
+        locker.receiveAndLock(pool);
     }
 }
