d32365f136d7922f2cdd7b0e9ea5e47567777e86
clemlak <clementlakhal@gmail.com>
2025-01-09 11:34:18 +0400

    test: add test_receiveAndLock test

--- test/unit/UniswapV2Locker.t.sol
+++ test/unit/UniswapV2Locker.t.sol
@@ -8,21 +8,40 @@ import { UniswapV2Migrator } from "src/UniswapV2Migrator.sol";
 import { Airlock } from "src/Airlock.sol";
 import { IUniswapV2Factory } from "src/interfaces/IUniswapV2Factory.sol";
 import { IUniswapV2Pair } from "src/interfaces/IUniswapV2Pair.sol";
+import { TestERC20 } from "v4-core/src/test/TestERC20.sol";
 
 contract UniswapV2LockerTest is Test {
     UniswapV2Locker public locker;
     UniswapV2Migrator public migrator;
+    IUniswapV2Pair public pool;
+
+    TestERC20 public tokenFoo;
+    TestERC20 public tokenBar;
 
     function setUp() public {
+        vm.createSelectFork(vm.envString("MAINNET_RPC_URL"), 21_093_509);
+
+        tokenFoo = new TestERC20(1e25);
+        tokenBar = new TestERC20(1e25);
+
         locker = new UniswapV2Locker(
             Airlock(payable(address(this))), IUniswapV2Factory(UNISWAP_V2_FACTORY_MAINNET), migrator
         );
+
+        pool = IUniswapV2Pair(
+            IUniswapV2Factory(UNISWAP_V2_FACTORY_MAINNET).createPair(address(tokenFoo), address(tokenBar))
+        );
+    }
+
+    function test_receiveAndLock() public {
+        tokenFoo.transfer(address(pool), 1e18);
+        tokenBar.transfer(address(pool), 1e18);
+        pool.mint(address(locker));
+        locker.receiveAndLock(address(pool));
     }
 
     function test_receiveAndLock_RevertsWhenNoBalanceToLock() public {
-        address pool = address(0x123);
-        vm.mockCall(pool, abi.encodeWithSelector(IUniswapV2Pair.balanceOf.selector, address(locker)), abi.encode(0));
         vm.expectRevert(NoBalanceToLock.selector);
-        locker.receiveAndLock(pool);
+        locker.receiveAndLock(address(pool));
     }
 }
