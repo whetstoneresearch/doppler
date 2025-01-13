622716088c28c19ed202e7566d61d92d37cfe4ac
Matt Czernik <matt.czernik@gmail.com>
2025-01-13 18:57:23 -0500

    Licenses (#282)
    * fix: license everywhere
    
    * fix: mit interfaces

--- src/Airlock.sol
+++ src/Airlock.sol
@@ -1,4 +1,4 @@
-// SPDX-License-Identifier: UNLICENSED
+// SPDX-License-Identifier: BUSL-1.1
 pragma solidity ^0.8.24;
 
 import { Ownable } from "@openzeppelin/access/Ownable.sol";
--- src/UniswapV4Initializer.sol
+++ src/UniswapV4Initializer.sol
@@ -1,4 +1,4 @@
-/// SPDX-License-Identifier: UNLICENSED
+/// SPDX-License-Identifier: BUSL-1.1
 pragma solidity ^0.8.13;
 
 import { IPoolManager, PoolKey, IHooks } from "v4-core/src/PoolManager.sol";
