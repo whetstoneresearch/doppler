8c381089695363d45ffb04d58022c42557116b4d
kinrezc <matt.czernik@gmail.com>
2025-01-22 20:29:30 -0500

    fix: deploy script

--- script/DeployFactoryUnichainSepolia.sol
+++ script/DeployFactoryUnichainSepolia.sol
@@ -3,6 +3,8 @@ pragma solidity ^0.8.24;
 
 import { Script, console2 } from "forge-std/Script.sol";
 import { IPoolManager } from "@v4-core/interfaces/IPoolManager.sol";
+import { PoolManager } from "@v4-core/PoolManager.sol";
+import { Deployers } from "@v4-core-test/utils/Deployers.sol";
 import { StateView } from "@v4-periphery/lens/StateView.sol";
 import { V4Quoter } from "@v4-periphery/lens/V4Quoter.sol";
 import { PoolSwapTest } from "@v4-core/test/PoolSwapTest.sol";
@@ -13,8 +15,14 @@ import { UniswapV2Migrator, IUniswapV2Router02, IUniswapV2Factory } from "src/Un
 import { UniswapV3Initializer, IUniswapV3Factory } from "src/UniswapV3Initializer.sol";
 import { UniswapV4Initializer, DopplerDeployer } from "src/UniswapV4Initializer.sol";
 import { CustomRouter2 } from "test/shared/CustomRouter2.sol";
+import { BasicRouter } from "test/shared/BasicRouter.sol";
+import { UniversalRouter } from "@universal-router/UniversalRouter.sol";
+import { RouterParameters } from "@universal-router/types/RouterParameters.sol";
 
-contract DeployDopplerV3FactoryUnichainSepolia is Script {
+bytes32 constant PAIR_INIT_CODE_HASH = 0x96e8ac4277198ff8b6f785478aa9a39f403cb768dd02cbee326c3e7da348845f;
+bytes32 constant POOL_INIT_CODE_HASH = 0xe34f199b19b2b4f47f68442619d555527d244f78a3297ea89325f843f87b8b54;
+
+contract DeployDopplerV3FactoryUnichainSepolia is Script, Deployers {
     Airlock airlock;
     TokenFactory tokenFactory;
     UniswapV4Initializer uniswapV4Initializer;
@@ -22,45 +30,63 @@ contract DeployDopplerV3FactoryUnichainSepolia is Script {
     GovernanceFactory governanceFactory;
     UniswapV2Migrator uniswapV2LiquidityMigrator;
     DopplerDeployer dopplerDeployer;
-    CustomRouter2 router;
+    UniversalRouter universalRouter;
+    BasicRouter router;
 
     function setUp() public { }
 
     address constant uniRouterV2 = 0x920b806E40A00E02E7D2b94fFc89860fDaEd3640;
     address constant uniFactoryV2 = 0x5C69bEe701ef814a2B6a3EDD4B1652CB9cc5aA6f;
 
-    address constant manager = 0xC81462Fec8B23319F288047f8A03A57682a35C1A;
-    address constant quoter = 0xfe6Cf50c4cfe801dd2AEf9c1B3ce24f551944df8;
     address constant stateView = 0xdE04C804dc75E90D8a64e5589092a1D6692EFA45;
     address constant uniRouter = 0x9e5A52f57b3038F1B8EeE45F28b3C1967e22799C;
+    address constant weth = 0x4200000000000000000000000000000000000006;
+    address constant permit2 = 0x000000000022D473030F116dDEE9F6B43aC78BA3;
 
     address v3CoreFactory = 0x1F98431c8aD98523631AE4a59f267346ea31F984;
     string public constant ENV_PRIVATE_KEY = "PRIVATE_KEY";
 
     function run() public {
         uint256 pk = vm.envUint(ENV_PRIVATE_KEY);
+
         vm.startBroadcast(pk);
 
+        RouterParameters memory params = RouterParameters({
+            permit2: permit2,
+            weth9: weth,
+            v2Factory: address(uniFactoryV2),
+            v3Factory: address(v3CoreFactory),
+            pairInitCodeHash: PAIR_INIT_CODE_HASH,
+            poolInitCodeHash: POOL_INIT_CODE_HASH,
+            v4PoolManager: address(manager),
+            v3NFTPositionManager: address(0),
+            v4PositionManager: address(0)
+        });
+
         address account = vm.addr(pk);
+
+        manager = new PoolManager(address(this));
+        console2.log("manager: ", address(manager), " as Address");
         airlock = new Airlock(address(account));
-        console2.log("Airlock: ", address(airlock));
+        console2.log("airlock: ", address(airlock), " as Address");
         tokenFactory = new TokenFactory(address(airlock));
-        console2.log("TokenFactory: ", address(tokenFactory));
+        console2.log("tokenFactory: ", address(tokenFactory), " as Address");
         dopplerDeployer = new DopplerDeployer(IPoolManager(manager));
+        console2.log("dopplerDeployer: ", address(dopplerDeployer), " as Address");
         uniswapV4Initializer = new UniswapV4Initializer(address(airlock), IPoolManager(manager), dopplerDeployer);
-        console2.log("UniswapV4Initializer: ", address(uniswapV4Initializer));
+        console2.log("v4Initializer: ", address(uniswapV4Initializer), " as Address");
         uniswapV3Initializer = new UniswapV3Initializer(address(airlock), IUniswapV3Factory(v3CoreFactory));
-        console2.log("UniswapV3Initializer: ", address(uniswapV3Initializer));
+        console2.log("v3Initializer: ", address(uniswapV3Initializer), " as Address");
         governanceFactory = new GovernanceFactory(address(airlock));
-        console2.log("GovernanceFactory: ", address(governanceFactory));
+        console2.log("governanceFactory: ", address(governanceFactory), " as Address");
         uniswapV2LiquidityMigrator =
             new UniswapV2Migrator(address(airlock), IUniswapV2Factory(uniFactoryV2), IUniswapV2Router02(uniRouterV2));
-        console2.log("Migrator: ", address(uniswapV2LiquidityMigrator));
-        console2.log("StateView: ", address(stateView));
-        console2.log("Quoter: ", address(quoter));
-        router = new CustomRouter2(PoolSwapTest(uniRouter), V4Quoter(quoter));
-        console2.log("CustomRouter: ", address(router));
-        console2.log(airlock.owner());
+        console2.log("migrator: ", address(uniswapV2LiquidityMigrator), " as Address");
+        console2.log("stateView: ", address(stateView), " as Address");
+        universalRouter = new UniversalRouter(params);
+        router = new BasicRouter(address(universalRouter));
+        console2.log("universalRouter: ", address(universalRouter), " as Address");
+        console2.log("basicRouter: ", address(router), " as Address");
 
         address[] memory modules = new address[](5);
         modules[0] = address(tokenFactory);
