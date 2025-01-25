ac14803707168101b151dbb0493998568ba2727f
saucepoint <saucepoint@protonmail.com>
2025-01-24 23:41:21 -0500

    temporarily patch out protocol fee calculation

--- src/Airlock.sol
+++ src/Airlock.sol
@@ -220,8 +220,11 @@ contract Airlock is Ownable {
         uint256 protocolLpFees0 = fees0 * 5 / 100;
         uint256 protocolLpFees1 = fees1 * 5 / 100;
 
-        uint256 protocolProceedsFees0 = fees0 > 0 ? (balance0 - fees0) / 1000 : 0;
-        uint256 protocolProceedsFees1 = fees1 > 0 ? (balance1 - fees1) / 1000 : 0;
+        // uint256 protocolProceedsFees0 = fees0 > 0 ? (balance0 - fees0) / 1000 : 0;
+        // uint256 protocolProceedsFees1 = fees1 > 0 ? (balance1 - fees1) / 1000 : 0;
+        // TODO: FIX PROTOCOL FEE CALCULATION
+        uint256 protocolProceedsFees0 = 0;
+        uint256 protocolProceedsFees1 = 0;
 
         uint256 protocolFees0 = protocolLpFees0 > protocolProceedsFees0 ? protocolLpFees0 : protocolProceedsFees0;
         uint256 protocolFees1 = protocolLpFees1 > protocolProceedsFees1 ? protocolLpFees1 : protocolProceedsFees1;
