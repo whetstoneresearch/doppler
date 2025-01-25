d7246510aab0718cf0541e8ced12a47384538265
saucepoint <saucepoint@protonmail.com>
2025-01-24 23:41:35 -0500

    test fee collection with native ether

--- test/unit/UniswapV4Initializer.t.sol
+++ test/unit/UniswapV4Initializer.t.sol
@@ -3,7 +3,7 @@ pragma solidity ^0.8.13;
 
 import { Test } from "forge-std/Test.sol";
 import { Deployers } from "@v4-core-test/utils/Deployers.sol";
-import { PoolManager } from "@v4-core/PoolManager.sol";
+import { PoolManager, IPoolManager } from "@v4-core/PoolManager.sol";
 import { TickMath } from "@v4-core/libraries/TickMath.sol";
 import { UniswapV4Initializer, DopplerDeployer } from "src/UniswapV4Initializer.sol";
 import { Airlock, ModuleState, CreateParams } from "src/Airlock.sol";
@@ -19,6 +19,12 @@ import {
     UNISWAP_V2_ROUTER_UNICHAIN_SEPOLIA
 } from "test/shared/Addresses.sol";
 import { mineV4, MineV4Params } from "test/shared/AirlockMiner.sol";
+import { Doppler } from "src/Doppler.sol";
+import { PoolSwapTest } from "@v4-core/test/PoolSwapTest.sol";
+import { PoolKey } from "@v4-core/types/PoolKey.sol";
+import { IHooks } from "@v4-core/interfaces/IHooks.sol";
+import { IERC20 } from "forge-std/interfaces/IERC20.sol";
+import { Currency, CurrencyLibrary } from "@v4-core/types/Currency.sol";
 
 uint256 constant DEFAULT_NUM_TOKENS_TO_SELL = 100_000e18;
 uint256 constant DEFAULT_MINIMUM_PROCEEDS = 100e18;
@@ -87,13 +93,15 @@ contract UniswapV4InitializerTest is Test, Deployers {
         states[2] = ModuleState.PoolInitializer;
         states[3] = ModuleState.LiquidityMigrator;
         airlock.setModuleState(modules, states);
+
+        swapRouter = new PoolSwapTest(manager);
     }
 
     function test_constructor() public view {
         assertEq(address(initializer.airlock()), address(airlock), "Wrong airlock");
     }
 
-    function test_v4initialize_success() public {
+    function test_v4initialize_success() public returns (address, address) {
         DopplerConfig memory config = DopplerConfig({
             numTokensToSell: DEFAULT_NUM_TOKENS_TO_SELL,
             minimumProceeds: DEFAULT_MINIMUM_PROCEEDS,
@@ -165,5 +173,104 @@ contract UniswapV4InitializerTest is Test, Deployers {
 
         assertEq(pool, hook, "Wrong pool");
         assertEq(asset, token, "Wrong asset");
+        return (hook, asset);
+    }
+
+    function test_v4_fee_collection() public {
+        (address hook, address asset) = test_v4initialize_success();
+        address numeraireAddress = Currency.unwrap(CurrencyLibrary.ADDRESS_ZERO);
+        Doppler doppler = Doppler(payable(hook));
+
+        IERC20(asset).approve(address(swapRouter), type(uint256).max);
+
+        key = PoolKey({
+            currency0: CurrencyLibrary.ADDRESS_ZERO,
+            currency1: Currency.wrap(asset),
+            fee: 3000, // hard coded in V4Initializer
+            tickSpacing: 8, // hard coded in V4Initializer
+            hooks: IHooks(hook)
+        });
+
+        // warp to starting time
+        vm.warp(block.timestamp + DEFAULT_STARTING_TIME);
+
+        // swap to generate fees
+        swapRouter.swap{ value: 0.1e18 }(
+            key,
+            IPoolManager.SwapParams({ zeroForOne: true, amountSpecified: -0.1e18, sqrtPriceLimitX96: MIN_PRICE_LIMIT }),
+            PoolSwapTest.TestSettings({ takeClaims: false, settleUsingBurn: false }),
+            ZERO_BYTES
+        );
+
+        swapRouter.swap(
+            key,
+            IPoolManager.SwapParams({ zeroForOne: false, amountSpecified: -0.01e18, sqrtPriceLimitX96: MAX_PRICE_LIMIT }),
+            PoolSwapTest.TestSettings({ takeClaims: false, settleUsingBurn: false }),
+            ZERO_BYTES
+        );
+
+        swapRouter.swap{ value: 0.1e18 }(
+            key,
+            IPoolManager.SwapParams({ zeroForOne: true, amountSpecified: -0.1e18, sqrtPriceLimitX96: MIN_PRICE_LIMIT }),
+            PoolSwapTest.TestSettings({ takeClaims: false, settleUsingBurn: false }),
+            ZERO_BYTES
+        );
+
+        swapRouter.swap(
+            key,
+            IPoolManager.SwapParams({ zeroForOne: false, amountSpecified: -0.01e18, sqrtPriceLimitX96: MAX_PRICE_LIMIT }),
+            PoolSwapTest.TestSettings({ takeClaims: false, settleUsingBurn: false }),
+            ZERO_BYTES
+        );
+
+        // mock out an early exit to test migration
+        _mockEarlyExit(doppler);
+
+        // migrate with native ether is successful
+        airlock.migrate(asset);
+
+        // protocol collects fees
+        address recipient = makeAddr("protocolFeeRecipient");
+        uint256 protocolFeesAsset = airlock.protocolFees(asset);
+        airlock.collectProtocolFees(recipient, asset, protocolFeesAsset);
+        assertGt(protocolFeesAsset, 0); // protocolFeesAsset > 0
+        assertEq(IERC20(asset).balanceOf(recipient), protocolFeesAsset);
+
+        uint256 protocolFeesNumeraire = airlock.protocolFees(numeraireAddress);
+        airlock.collectProtocolFees(recipient, numeraireAddress, protocolFeesNumeraire);
+        assertGt(protocolFeesNumeraire, 0); // protocolFeesNumeraire > 0
+        assertEq(recipient.balance, protocolFeesNumeraire);
+
+        // integrator collects fees
+        address integratorRecipient = makeAddr("integratorFeeRecipient");
+        uint256 integratorFeesAsset = airlock.integratorFees(address(this), asset);
+        airlock.collectIntegratorFees(integratorRecipient, asset, integratorFeesAsset);
+        assertGt(integratorFeesAsset, 0); // integratorFeesAsset > 0
+        assertEq(IERC20(asset).balanceOf(integratorRecipient), integratorFeesAsset);
+
+        uint256 integratorFeesNumeraire = airlock.integratorFees(address(this), numeraireAddress);
+        airlock.collectIntegratorFees(integratorRecipient, numeraireAddress, integratorFeesNumeraire);
+        assertGt(integratorFeesNumeraire, 0); // integratorFeesNumeraire > 0
+        assertEq(integratorRecipient.balance, integratorFeesNumeraire);
+    }
+
+    function _mockEarlyExit(
+        Doppler doppler
+    ) internal {
+        // storage slot of earlyExit variable
+        // via `forge inspect Doppler storage`
+        bytes32 EARLY_EXIT_SLOT = bytes32(uint256(0));
+
+        vm.record();
+        doppler.earlyExit();
+        (bytes32[] memory reads,) = vm.accesses(address(doppler));
+        assertEq(reads.length, 1, "wrong reads");
+        assertEq(reads[0], EARLY_EXIT_SLOT, "wrong slot");
+
+        // need to offset the boolean (0x01) by 1 byte since `insufficientProceeds`
+        // and `earlyExit` share slot0
+        vm.store(address(doppler), EARLY_EXIT_SLOT, bytes32(uint256(0x0100)));
+
+        assertTrue(doppler.earlyExit(), "early exit should be true");
     }
 }
