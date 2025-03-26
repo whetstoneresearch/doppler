fca1428d4650922c393449894cfa71abc07f8ffb
clemlak <39790678+clemlak@users.noreply.github.com>
2025-03-26 21:11:28 +0800

    build: update BundlerDeployer script

--- script/Bundler.s.sol
+++ script/Bundler.s.sol
@@ -1,11 +1,17 @@
-import { Bundler } from "src/Bundler.sol";
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity ^0.8.24;
+
 import { Script, console } from "forge-std/Script.sol";
+import { UniversalRouter } from "@universal-router/UniversalRouter.sol";
+import { IQuoterV2 } from "@v3-periphery/interfaces/IQuoterV2.sol";
+import { Airlock } from "src/Airlock.sol";
+import { Bundler } from "src/Bundler.sol";
 
 contract BundlerDeployer is Script {
     function run() public {
-        address payable airlock = payable(0xe7dfbd5b0A2C3B4464653A9beCdc489229eF090E);
-        address payable router = payable(0x95273d871c8156636e114b63797d78D7E1720d81);
-        address quoter = 0xC5290058841028F1614F3A6F0F5816cAd0df5E27;
+        Airlock airlock = Airlock(payable(0xe7dfbd5b0A2C3B4464653A9beCdc489229eF090E));
+        UniversalRouter router = UniversalRouter(payable(0x95273d871c8156636e114b63797d78D7E1720d81));
+        IQuoterV2 quoter = IQuoterV2(0xC5290058841028F1614F3A6F0F5816cAd0df5E27);
 
         vm.startBroadcast();
         Bundler bundler = new Bundler(airlock, router, quoter);
