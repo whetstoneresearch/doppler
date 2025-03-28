4acaf25916032914945f77c686969d07600478c3
clemlak <39790678+clemlak@users.noreply.github.com>
2025-03-28 17:44:08 +0800

    chore: update NatSpec

--- src/Bundler.sol
+++ src/Bundler.sol
@@ -6,7 +6,10 @@ import { Airlock, CreateParams } from "src/Airlock.sol";
 import { UniversalRouter } from "@universal-router/UniversalRouter.sol";
 import { IQuoterV2 } from "@v3-periphery/interfaces/IQuoterV2.sol";
 
+/// @dev Thrown when an invalid address is passed as a contructor parameter
 error InvalidAddresses();
+
+/// @dev Thrown when the asset address doesn't match the predicted one
 error InvalidOutputToken();
 
 /**
@@ -14,10 +17,20 @@ error InvalidOutputToken();
  * @custom:security-contact security@whetstone.cc
  */
 contract Bundler {
+    /// @notice Address of the Airlock contract
     Airlock public immutable airlock;
+
+    /// @notice Address of the Universal Router contract
     UniversalRouter public immutable router;
+
+    /// @notice Address of the QuoterV2 contract
     IQuoterV2 public immutable quoter;
 
+    /**
+     * @param airlock_ Immutable address of the Airlock contract
+     * @param router_ Immutable address of the Universal Router contract
+     * @param quoter_ Immutable address of the QuoterV2 contract
+     */
     constructor(Airlock airlock_, UniversalRouter router_, IQuoterV2 quoter_) {
         if (address(airlock_) == address(0) || address(router_) == address(0)) {
             revert InvalidAddresses();
@@ -27,6 +40,12 @@ contract Bundler {
         quoter = IQuoterV2(quoter_);
     }
 
+    /**
+     * @notice Simulates a bundle operation with an exact output amount
+     * @param createData Creation data to pass to the Airlock contract
+     * @param params Exact output parameters to pass to the QuoterV2 contract
+     * @return amountIn Amount of input token required to receive the exact output amount
+     */
     function simulateBundleExactOut(
         CreateParams calldata createData,
         IQuoterV2.QuoteExactOutputSingleParams calldata params
@@ -38,6 +57,12 @@ contract Bundler {
         (amountIn,,,) = quoter.quoteExactOutputSingle(params);
     }
 
+    /**
+     * @notice Simulates a bundle operation with an exact input amount
+     * @param createData Creation data to pass to the Airlock contract
+     * @param params Exact input parameters to pass to the QuoterV2 contract
+     * @return amountOut Amount of output token received from the exact input amount
+     */
     function simulateBundleExactIn(
         CreateParams calldata createData,
         IQuoterV2.QuoteExactInputSingleParams calldata params
@@ -50,10 +75,10 @@ contract Bundler {
     }
 
     /**
-     * @notice Bundles the creation of an asset via the Airlock contract and a buy operation via the universal router
+     * @notice Bundles the creation of an asset via the Airlock contract and a buy operation via the Universal Router
      * @param createData Creation data to pass to the Airlock contract
-     * @param commands encoded universal router commands
-     * @param inputs universal router vm calldata
+     * @param commands Encoded commands for the Universal Router
+     * @param inputs Encoded inputs for the Universal Router
      */
     function bundle(
         CreateParams calldata createData,
