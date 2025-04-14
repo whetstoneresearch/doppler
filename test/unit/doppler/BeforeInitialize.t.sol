befe948110f79352169bacc60833a5b254d9e8d9
clemlak <39790678+clemlak@users.noreply.github.com>
2025-04-14 15:37:46 +0400

    test: add test_beforeInitialize_RevertsWhenInvalidGamma

--- test/unit/doppler/BeforeInitialize.t.sol
+++ test/unit/doppler/BeforeInitialize.t.sol
@@ -5,7 +5,7 @@ import { BaseTest } from "test/shared/BaseTest.sol";
 import { PoolKey } from "@v4-core/types/PoolKey.sol";
 import { Currency } from "@v4-core/types/Currency.sol";
 import { IHooks } from "@v4-core/interfaces/IHooks.sol";
-import { MAX_TICK_SPACING, InvalidTickSpacing, AlreadyInitialized } from "src/Doppler.sol";
+import { MAX_TICK_SPACING, InvalidTickSpacing, AlreadyInitialized, InvalidGamma } from "src/Doppler.sol";
 
 contract BeforeInitializeTest is BaseTest {
     function test_beforeInitialize_RevertsWhenAlreadyInitialized() public {
@@ -37,6 +37,24 @@ contract BeforeInitializeTest is BaseTest {
                 currency1: Currency.wrap(address(0)),
                 fee: 0,
                 tickSpacing: MAX_TICK_SPACING + 1,
+                hooks: IHooks(address(0))
+            }),
+            0
+        );
+    }
+
+    function test_beforeInitialize_RevertsWhenInvalidGamma() public {
+        hook.resetInitialized();
+        vm.prank(address(hook.poolManager()));
+
+        vm.expectRevert(InvalidGamma.selector);
+        hook.beforeInitialize(
+            address(0),
+            PoolKey({
+                currency0: Currency.wrap(address(0)),
+                currency1: Currency.wrap(address(0)),
+                fee: 0,
+                tickSpacing: 30,
                 hooks: IHooks(address(0))
             }),
             0
