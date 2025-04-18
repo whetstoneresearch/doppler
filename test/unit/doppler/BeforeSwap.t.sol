09bfb45002b26872af3e1f9403c49742d2db26e0
clemlak <39790678+clemlak@users.noreply.github.com>
2025-04-18 14:50:47 +0400

    test: add test_beforeSwap_RevertsWhenEarlyExit

--- test/unit/doppler/BeforeSwap.t.sol
+++ test/unit/doppler/BeforeSwap.t.sol
@@ -4,10 +4,23 @@ pragma solidity ^0.8.24;
 import { IPoolManager } from "@v4-core/interfaces/IPoolManager.sol";
 import { ImmutableState } from "@v4-periphery/base/ImmutableState.sol";
 import { BaseTest } from "test/shared/BaseTest.sol";
+import { MaximumProceedsReached } from "src/Doppler.sol";
 
 contract BeforeSwapTest is BaseTest {
     function test_beforeSwap_RevertsIfNotPoolManager() public {
         vm.expectRevert(ImmutableState.NotPoolManager.selector);
+        hook.beforeSwap(
+            address(this),
+            key,
+            IPoolManager.SwapParams({ zeroForOne: true, amountSpecified: 100e18, sqrtPriceLimitX96: SQRT_RATIO_2_1 }),
+            ""
+        );
+    }
+
+    function test_beforeSwap_RevertsWhenEarlyExit() public {
+        hook.setEarlyExit();
+        vm.prank(address(manager));
+        vm.expectRevert(MaximumProceedsReached.selector);
         hook.beforeSwap(
             address(this),
             key,
