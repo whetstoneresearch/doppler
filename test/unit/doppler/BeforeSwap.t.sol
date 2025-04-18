e72dd2c7b963a6e5eab75344f111a1f628bada35
clemlak <39790678+clemlak@users.noreply.github.com>
2025-04-18 14:37:31 +0400

    test: clean up file

--- test/unit/doppler/BeforeSwap.t.sol
+++ test/unit/doppler/BeforeSwap.t.sol
@@ -5,13 +5,7 @@ import { IPoolManager } from "@v4-core/interfaces/IPoolManager.sol";
 import { ImmutableState } from "@v4-periphery/base/ImmutableState.sol";
 import { BaseTest } from "test/shared/BaseTest.sol";
 
-/// @dev forge test -vvv --mc DopplerBeforeSwapTest --via-ir
-/// TODO: I duplicated this from the test file just to test this out for now.
 contract BeforeSwapTest is BaseTest {
-    // =========================================================================
-    //                         beforeSwap Unit Tests
-    // =========================================================================
-
     function testBeforeSwap_RevertsIfNotPoolManager() public {
         vm.expectRevert(ImmutableState.NotPoolManager.selector);
         hook.beforeSwap(
