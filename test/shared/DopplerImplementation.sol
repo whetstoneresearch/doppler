f78f0ceee33475e38b67baf3e54d25119e7e8db6
clemlak <39790678+clemlak@users.noreply.github.com>
2025-05-20 15:58:46 +0400

    test: add alignTickDeltaWithTickSpacing getter

--- test/shared/DopplerImplementation.sol
+++ test/shared/DopplerImplementation.sol
@@ -107,6 +107,10 @@ contract DopplerImplementation is Doppler {
         return _alignComputedTickWithTickSpacing(tick, tickSpacing);
     }
 
+    function alignTickDeltaWithTickSpacing(int256 tick, int256 tickSpacing) public view returns (int256) {
+        return _alignTickDeltaWithTickSpacing(tick, tickSpacing);
+    }
+
     function computeLowerSlugData(
         PoolKey memory key,
         uint256 requiredProceeds,
