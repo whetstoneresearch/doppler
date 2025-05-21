688cf6f31eeb5114d2f3da0ac04eeeea258aa9a7
clemlak <39790678+clemlak@users.noreply.github.com>
2025-05-21 18:02:19 +0400

    build: merge DeployLensQuoterScript deployment  script with V4Script

--- script/deployV4/DeployV4.s.sol
+++ script/deployV4/DeployV4.s.sol
@@ -2,12 +2,16 @@
 pragma solidity ^0.8.24;
 
 import { Script, console } from "forge-std/Script.sol";
+import { IPoolManager } from "@v4-core/interfaces/IPoolManager.sol";
+import { IStateView } from "@v4-periphery/lens/StateView.sol";
+import { DopplerLensQuoter } from "src/lens/DopplerLens.sol";
 import { Airlock } from "src/Airlock.sol";
 import { UniswapV4Initializer, DopplerDeployer, IPoolManager } from "src/UniswapV4Initializer.sol";
 
 struct V4ScriptData {
     address airlock;
     address poolManager;
+    address stateView;
 }
 
 /**
@@ -23,21 +27,23 @@ abstract contract DeployV4Script is Script {
         console.log(unicode"🚀 Deploying V4 on chain %s with sender %s...", vm.toString(block.chainid), msg.sender);
 
         vm.startBroadcast();
-        (DopplerDeployer dopplerDeployer, UniswapV4Initializer uniswapV4Initializer) = _deployV4(_scriptData);
+
+        DopplerDeployer dopplerDeployer = new DopplerDeployer(IPoolManager(_scriptData.poolManager));
+        UniswapV4Initializer uniswapV4Initializer =
+            new UniswapV4Initializer(_scriptData.airlock, IPoolManager(_scriptData.poolManager), dopplerDeployer);
+        DopplerLensQuoter quoter =
+            new DopplerLensQuoter(IPoolManager(_scriptData.poolManager), IStateView(_scriptData.stateView));
 
         console.log(unicode"✨ Contracts were successfully deployed!");
 
-        console.log("DopplerDeployer: ", address(dopplerDeployer));
-        console.log("UniswapV4Initializer: ", address(uniswapV4Initializer));
+        console.log("+----------------------------+--------------------------------------------+");
+        console.log("| Contract Name              | Address                                    |");
+        console.log("+----------------------------+--------------------------------------------+");
+        console.log("| UniswapV4Initializer       | %s |", address(uniswapV4Initializer));
+        console.log("| DopplerDeployer            | %s |", address(dopplerDeployer));
+        console.log("| DopplerLensQuoter          | %s |", address(quoter));
+        console.log("+----------------------------+--------------------------------------------+");
 
         vm.stopBroadcast();
-    }
-
-    function _deployV4(
-        V4ScriptData memory scriptData
-    ) internal returns (DopplerDeployer dopplerDeployer, UniswapV4Initializer uniswapV4Initializer) {
-        dopplerDeployer = new DopplerDeployer(IPoolManager(scriptData.poolManager));
-        uniswapV4Initializer =
-            new UniswapV4Initializer(scriptData.airlock, IPoolManager(scriptData.poolManager), dopplerDeployer);
     }
 }
--- script/deployV4/DeployV4Base.s.sol
+++ script/deployV4/DeployV4Base.s.sol
@@ -7,7 +7,8 @@ contract DeployV4Base is DeployV4Script {
     function setUp() public override {
         _scriptData = V4ScriptData({
             airlock: 0x660eAaEdEBc968f8f3694354FA8EC0b4c5Ba8D12,
-            poolManager: 0x498581fF718922c3f8e6A244956aF099B2652b2b
+            poolManager: 0x498581fF718922c3f8e6A244956aF099B2652b2b,
+            stateView: 0xA3c0c9b65baD0b08107Aa264b0f3dB444b867A71
         });
     }
 }
