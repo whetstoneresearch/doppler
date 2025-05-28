ede12ce4c204b204df5e6c3efdc6d39173c97f49
clemlak <39790678+clemlak@users.noreply.github.com>
2025-05-28 09:59:19 +0400

    test: add generateConfig (wip)

--- test/shared/HookConfigs.sol
+++ test/shared/HookConfigs.sol
@@ -1,6 +1,10 @@
 // SPDX-License-Identifier: UNLICENSED
 pragma solidity ^0.8.24;
 
+import { TickMath } from "@v4-core/libraries/TickMath.sol";
+import { SwapMath } from "@v4-core/libraries/SwapMath.sol";
+import { MAX_TICK_SPACING, MAX_SWAP_FEE, MAX_PRICE_DISCOVERY_SLUGS } from "src/Doppler.sol";
+
 struct HookConfig {
     uint256 numTokensToSell;
     uint256 minimumProceeds;
@@ -18,6 +22,34 @@ struct HookConfig {
 }
 
 library HookConfigs {
+    function isValidConfig(
+        HookConfig memory config
+    ) internal pure returns (bool) { }
+
+    function generateConfig(
+        uint256 seed
+    ) internal pure returns (HookConfig memory config) {
+        bool isToken0 = seed % 2 == 0;
+        int24 tickSpacing = (int24(uint24(seed)) % MAX_TICK_SPACING) + 1;
+        uint256 numPDSlugs = seed % MAX_PRICE_DISCOVERY_SLUGS + 1;
+        uint256 startingTime = seed % type(uint256).max / 2;
+        uint256 epochLength = seed % 10 days + 1 minutes;
+        uint256 totalEpochs = (seed % 200) + 1;
+        uint256 endingTime = startingTime + (totalEpochs * epochLength);
+        int24 gamma = 0;
+        int24 startingTick = TickMath.MIN_TICK + (int24(uint24(seed)) % (TickMath.MAX_TICK - TickMath.MIN_TICK));
+        int24 endingTick = startingTick + (int24(uint24(seed)) % (TickMath.MAX_TICK - startingTick));
+
+        if (isToken0) {
+            startingTick = -startingTick;
+            endingTick = -endingTick;
+        }
+
+        uint256 numTokensToSell = seed % type(uint64).max + 1;
+        uint256 minimumProceeds = (seed % 100 ether) + 1 ether;
+        uint256 maximumProceeds = minimumProceeds + (seed % 10 ether) + 1 ether;
+    }
+
     function DEFAULT_CONFIG_0() internal pure returns (HookConfig memory) {
         return HookConfig({
             numTokensToSell: 6e28,
