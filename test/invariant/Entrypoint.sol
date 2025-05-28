e4676a29b34b791f6aa6748d26fa9eaf8b791661
clemlak <39790678+clemlak@users.noreply.github.com>
2025-05-28 09:59:51 +0400

    test: clean up Entrypoint

--- test/invariant/Entrypoint.sol
+++ test/invariant/Entrypoint.sol
@@ -1,14 +1,13 @@
 // SPDX-License-Identifier: UNLICENSED
 pragma solidity ^0.8.24;
 
-import { Test } from "forge-std/Test.sol";
-import { console } from "forge-std/console.sol";
+import { HookConfigs } from "test/shared/HookConfigs.sol";
 
 uint256 constant TOTAL_WEIGHTS = 100;
 
 contract Entrypoint {
-    bytes4[] public _selectors;
-    mapping(bytes4 => uint256) public _weights;
+    bytes4[] internal _selectors;
+    mapping(bytes4 => uint256) internal _weights;
 
     function setSelectorWeights(bytes4[] memory selectors, uint256[] memory weights) external {
         require(selectors.length == weights.length, "Arrays mismatch");
@@ -37,46 +36,5 @@ contract Entrypoint {
                 break;
             }
         }
-    }
-}
-
-contract EntrypointTest is Test {
-    Entrypoint public entrypoint;
-
-    mapping(bytes4 => uint256) public results;
-
-    function setUp() public {
-        entrypoint = new Entrypoint();
-    }
-
-    function test_entrypoint(
-        uint256 rng
-    ) public {
-        vm.assume(rng > 0);
-        uint256 seed = type(uint256).max % rng;
-
-        bytes4[] memory selectors = new bytes4[](3);
-        uint256[] memory weights = new uint256[](3);
-
-        selectors[0] = bytes4(hex"beefbeef");
-        selectors[1] = bytes4(hex"cafecafe");
-        selectors[2] = bytes4(hex"deadbeef");
-
-        weights[0] = 50;
-        weights[1] = 25;
-        weights[2] = 25;
-
-        entrypoint.setSelectorWeights(selectors, weights);
-
-        uint256 runs = 1000;
-
-        for (uint256 i; i < runs; i++) {
-            bytes4 selector = entrypoint.entrypoint(seed + i);
-            results[selector] += 1;
-        }
-
-        assertEq(results[bytes4(hex"beefbeef")], weights[0] * runs / TOTAL_WEIGHTS, "beefbeef");
-        assertEq(results[bytes4(hex"cafecafe")], weights[1] * runs / TOTAL_WEIGHTS, "cafecafe");
-        assertEq(results[bytes4(hex"deadbeef")], weights[2] * runs / TOTAL_WEIGHTS, "deadbeef");
     }
 }
