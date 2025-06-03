67481807e8d36449a84d102c03646a94c8fcbb86
clemlak <39790678+clemlak@users.noreply.github.com>
2025-06-03 16:10:22 +0400

    feat: update V4Migrator

--- src/UniswapV4Migrator.sol
+++ src/UniswapV4Migrator.sol
@@ -1,7 +1,7 @@
 // SPDX-License-Identifier: BUSL-1.1
 pragma solidity ^0.8.24;
 
-import { SafeTransferLib, ERC20 } from "@solmate/utils/SafeTransferLib.sol";
+import { ERC20 } from "@solmate/utils/SafeTransferLib.sol";
 import { Actions } from "@v4-periphery/libraries/Actions.sol";
 import { IPoolManager } from "@v4-core/interfaces/IPoolManager.sol";
 import { PositionManager } from "@v4-periphery/PositionManager.sol";
@@ -15,10 +15,11 @@ import { TickMath } from "@v4-core/libraries/TickMath.sol";
 import { LiquidityAmounts } from "@v4-periphery/libraries/LiquidityAmounts.sol";
 import { ILiquidityMigrator } from "src/interfaces/ILiquidityMigrator.sol";
 import { ImmutableAirlock } from "src/base/ImmutableAirlock.sol";
+import { BeneficiaryData, StreamableFeesLocker } from "src/StreamableFeesLocker.sol";
 
 struct AssetData {
     PoolKey poolKey;
-    address integrator;
+    BeneficiaryData[] beneficiaries;
 }
 
 contract UniswapV4Migrator is ILiquidityMigrator, ImmutableAirlock {
@@ -27,14 +28,21 @@ contract UniswapV4Migrator is ILiquidityMigrator, ImmutableAirlock {
 
     IPoolManager public immutable poolManager;
     PositionManager public immutable positionManager;
+    StreamableFeesLocker public immutable locker;
 
-    mapping(address token0 => mapping(address token1 => PoolKey poolKey)) public getPoolKeyForPair;
+    mapping(address token0 => mapping(address token1 => AssetData data)) public getAssetData;
 
     receive() external payable { }
 
-    constructor(address airlock_, address poolManager_, address payable positionManager_) ImmutableAirlock(airlock_) {
+    constructor(
+        address airlock_,
+        address poolManager_,
+        address payable positionManager_,
+        StreamableFeesLocker locker_
+    ) ImmutableAirlock(airlock_) {
         poolManager = IPoolManager(poolManager_);
         positionManager = PositionManager(positionManager_);
+        locker = locker_;
     }
 
     function initialize(
@@ -42,7 +50,8 @@ contract UniswapV4Migrator is ILiquidityMigrator, ImmutableAirlock {
         address numeraire,
         bytes calldata liquidityMigratorData
     ) external onlyAirlock returns (address) {
-        (uint24 fee, int24 tickSpacing) = abi.decode(liquidityMigratorData, (uint24, int24));
+        (uint24 fee, int24 tickSpacing, BeneficiaryData[] memory data) =
+            abi.decode(liquidityMigratorData, (uint24, int24, BeneficiaryData[]));
 
         PoolKey memory poolKey = PoolKey({
             currency0: asset < numeraire ? Currency.wrap(asset) : Currency.wrap(numeraire),
@@ -52,7 +61,8 @@ contract UniswapV4Migrator is ILiquidityMigrator, ImmutableAirlock {
             tickSpacing: tickSpacing
         });
 
-        getPoolKeyForPair[Currency.unwrap(poolKey.currency0)][Currency.unwrap(poolKey.currency1)] = poolKey;
+        getAssetData[Currency.unwrap(poolKey.currency0)][Currency.unwrap(poolKey.currency1)] =
+            AssetData({ poolKey: poolKey, beneficiaries: data });
 
         return address(0);
     }
@@ -63,11 +73,8 @@ contract UniswapV4Migrator is ILiquidityMigrator, ImmutableAirlock {
         address token1,
         address recipient
     ) external payable onlyAirlock returns (uint256 liquidity) {
-        // TODO: Define these guys
-        address timelock;
-        address protocolLocker;
-
-        PoolKey memory poolKey = getPoolKeyForPair[token0][token1];
+        AssetData memory assetData = getAssetData[token0][token1];
+        PoolKey memory poolKey = assetData.poolKey;
 
         // Let's check if the pool was initialized
         (uint160 currentSqrtPriceX96,,,) = poolManager.getSlot0(poolKey.toId());
@@ -116,18 +123,18 @@ contract UniswapV4Migrator is ILiquidityMigrator, ImmutableAirlock {
 
         require(liquidity > 0, "UniswapV4Migrator: ZERO_LIQUIDITY");
 
-        uint256 protocolLockerLiquidity = liquidity / 20;
-        uint256 timeLockLiquidity = liquidity - protocolLockerLiquidity;
+        uint256 lockedLiquidity = liquidity / 10;
+        uint256 timeLockLiquidity = liquidity - lockedLiquidity;
 
         // Liquidity for the protocol locker
         params[0] = abi.encode(
             poolKey,
             lowerTick,
             upperTick,
-            uint128(protocolLockerLiquidity),
-            uint128(balance0) / 20,
-            uint128(balance1) / 20,
-            protocolLocker,
+            uint128(lockedLiquidity),
+            uint128(balance0) / 10,
+            uint128(balance1) / 10,
+            address(this),
             new bytes(0)
         );
 
@@ -139,7 +146,7 @@ contract UniswapV4Migrator is ILiquidityMigrator, ImmutableAirlock {
             uint128(timeLockLiquidity),
             uint128(balance0),
             uint128(balance1),
-            timelock,
+            recipient,
             new bytes(0)
         );
 
@@ -157,8 +164,15 @@ contract UniswapV4Migrator is ILiquidityMigrator, ImmutableAirlock {
             abi.encode(actions, params), block.timestamp
         );
 
-        uint256 tokenId = positionManager.nextTokenId() - 1;
+        positionManager.safeTransferFrom(
+            address(this), address(locker), positionManager.nextTokenId() - 2, abi.encode(assetData.beneficiaries)
+        );
 
-        positionManager.transferFrom(address(this), recipient, tokenId);
+        if (poolKey.currency0.balanceOfSelf() > 0) {
+            poolKey.currency0.transfer(recipient, poolKey.currency0.balanceOfSelf());
+        }
+        if (poolKey.currency1.balanceOfSelf() > 0) {
+            poolKey.currency1.transfer(recipient, poolKey.currency1.balanceOfSelf());
+        }
     }
 }
