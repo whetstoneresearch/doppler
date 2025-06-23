68b9f3476a976398e985bea27ab1d69b5bec693f
clemlak <39790678+clemlak@users.noreply.github.com>
2025-06-24 00:26:47 +0400

    build: set create2 factory address as hook deployer

--- script/deploy/Deploy.s.sol
+++ script/deploy/Deploy.s.sol
@@ -179,7 +179,7 @@ abstract contract DeployScript is Script {
             MineV4MigratorHookParams({
                 poolManager: _scriptData.poolManager,
                 migrator: precomputedUniswapV4Migrator,
-                hookDeployer: msg.sender
+                hookDeployer: 0x4e59b44847b379578588920cA78FbF26c0B4956C
             })
         );
 
