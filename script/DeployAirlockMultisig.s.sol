ee443ad1377523585f5b3248e05cd4d740c1970b
clemlak <39790678+clemlak@users.noreply.github.com>
2025-07-11 18:03:44 +0400

    build: update AirlockMultisig deployment script

--- script/DeployAirlockMultisig.s.sol
+++ script/DeployAirlockMultisig.s.sol
@@ -1,23 +1,37 @@
 // SPDX-License-Identifier: UNLICENSED
 pragma solidity ^0.8.24;
 
-import { Script, console } from "forge-std/Script.sol";
+import { Script } from "forge-std/Script.sol";
 import { Airlock } from "src/Airlock.sol";
 import { AirlockMultisig } from "test/shared/AirlockMultisig.sol";
 
-contract DeployAirlockMultisigScript is Script {
+struct ScriptData {
+    uint256 chainId;
+    address airlock;
+    address[] signers;
+}
+
+abstract contract DeployAirlockMultisigScript is Script {
+    ScriptData internal _scriptData;
+
+    function setUp() public virtual;
+
     function run() public {
-        console.log(unicode"🚀 Deploying on chain %s with sender %s...", vm.toString(block.chainid), msg.sender);
+        vm.startBroadcast();
+        require(_scriptData.chainId == block.chainid, "Incorrect chainId");
+        AirlockMultisig multisig = new AirlockMultisig(Airlock(payable(_scriptData.airlock)), _scriptData.signers);
+        vm.stopBroadcast();
+    }
+}
 
-        address[] memory signers = new address[](1);
+/// @dev forge script DeployAirlockMultisigBaseSepoliaScript --private-key $PRIVATE_KEY --verify --rpc-url $BASE_SEPOLIA_RPC_URL --slow --broadcast
+contract DeployAirlockMultisigBaseSepoliaScript is DeployAirlockMultisigScript {
+    function setUp() public override {
+        address[] memory signers = new address[](2);
         signers[0] = msg.sender;
+        signers[1] = 0x88C23B886580FfAd04C66055edB6c777f5F74a08;
 
-        try vm.promptAddress("Enter the address of the Airlock contract:") returns (address airlock) {
-            vm.startBroadcast();
-            AirlockMultisig multisig = new AirlockMultisig(Airlock(payable(airlock)), signers);
-            vm.stopBroadcast();
-        } catch {
-            revert("Airlock contract address is required.");
-        }
+        _scriptData =
+            ScriptData({ chainId: 84_532, airlock: 0x3411306Ce66c9469BFF1535BA955503c4Bde1C6e, signers: signers });
     }
 }
