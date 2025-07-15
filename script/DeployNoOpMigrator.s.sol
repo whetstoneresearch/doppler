51f9aec44d304a0d7c3ac92ccf24c9d0a45c0efe
clemlak <39790678+clemlak@users.noreply.github.com>
2025-07-15 19:59:30 +0200

    chore: add deployment commands

--- script/DeployNoOpMigrator.s.sol
+++ script/DeployNoOpMigrator.s.sol
@@ -23,12 +23,14 @@ abstract contract DeployNoOpMigratorScript is Script {
     }
 }
 
+/// @dev forge script --private-key $PRIVATE_KEY --verify --slow --broadcast --rpc-url $BASE_MAINNET_RPC_URL
 contract DeployNoOpMigratorBaseScript is DeployNoOpMigratorScript {
     function setUp() public override {
         _scriptData = ScriptData({ airlock: 0x660eAaEdEBc968f8f3694354FA8EC0b4c5Ba8D12, chainId: 8453 });
     }
 }
 
+/// @dev forge script --private-key $PRIVATE_KEY --verify --slow --broadcast --rpc-url $BASE_SEPOLIA_RPC_URL
 contract DeployNoOpMigratorBaseSepoliaScript is DeployNoOpMigratorScript {
     function setUp() public override {
         _scriptData = ScriptData({ airlock: 0x3411306Ce66c9469BFF1535BA955503c4Bde1C6e, chainId: 84_532 });
