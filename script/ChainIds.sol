9a64d9a952020db013062d453c060b7da7d1f868
clemlak <39790678+clemlak@users.noreply.github.com>
2025-07-29 15:24:55 +0200

    build: add chain ids

---
+++ script/ChainIds.sol
@@ -0,0 +1,11 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity ^0.8.24;
+
+library ChainIds {
+    uint256 constant ETH_MAINNET = 1;
+    uint256 constant UNICHAIN_MAINNET = 130;
+    uint256 constant UNICHAIN_SEPOLIA = 1301;
+    uint256 constant INK_MAINNET = 57_073;
+    uint256 constant BASE_MAINNET = 8453;
+    uint256 constant BASE_SEPOLIA = 84_532;
+}
