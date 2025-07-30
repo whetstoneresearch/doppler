c8b79fbe9e5e65585c150e61c945bff4c17c8cc7
clemlak <39790678+clemlak@users.noreply.github.com>
2025-07-30 10:08:13 +0200

    build: add UniV2 deployment script

---
+++ script/legacy/DeployUniV2.s.sol
@@ -0,0 +1,54 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity ^0.8.24;
+
+import { Script } from "forge-std/Script.sol";
+import { ChainIds } from "script/ChainIds.sol";
+
+struct ScriptData {
+    uint256 chainId;
+    address weth;
+}
+
+/// @dev Deploys Uniswap V2 factory and router contracts. It might happen that these contracts
+/// are not present on a recent testnet, so this script can be used to deploy them.
+abstract contract DeployUniV2Script is Script {
+    ScriptData internal _scriptData;
+
+    function setUp() public virtual;
+
+    function run() public {
+        require(_scriptData.chainId == block.chainid, "Invalid chainId");
+        vm.startBroadcast();
+
+        address uniswapV2Factory = _deployCode(
+            abi.encodePacked(
+                vm.parseBytes(vm.readFile("./script/utils/uniswapV2Factory.bytecode")), abi.encode(address(0))
+            )
+        );
+
+        address uniswapV2Router02 = _deployCode(
+            abi.encodePacked(
+                vm.parseBytes(vm.readFile("./script/utils/uniswapV2Router02.bytecode")),
+                abi.encode(uniswapV2Factory, _scriptData.weth)
+            )
+        );
+
+        vm.stopBroadcast();
+    }
+
+    function _deployCode(
+        bytes memory creationCode
+    ) internal returns (address deployedTo) {
+        assembly {
+            deployedTo := create(0, add(creationCode, 0x20), mload(creationCode))
+        }
+        require(deployedTo != address(0), "Deploy failed");
+    }
+}
+
+/// @dev forge script DeployUniV2DomaTestnetScript --rpc-url $DOMA_TESTNET_RPC_URL --broadcast --slow --private-key $PRIVATE_KEY
+contract DeployUniV2DomaTestnetScript is DeployUniV2Script {
+    function setUp() public override {
+        _scriptData = ScriptData({ weth: 0x4200000000000000000000000000000000000006, chainId: ChainIds.DOMA_TESTNET });
+    }
+}
