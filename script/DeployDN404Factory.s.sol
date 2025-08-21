5adeb032ca901ea4141afe074926933503d000c9
z80 <z80@ophy.xyz>
2025-08-21 15:07:23 -0400

    add DN404Factory deploy script

---
+++ script/DeployDN404Factory.s.sol
@@ -0,0 +1,37 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity ^0.8.24;
+
+import { Script } from "forge-std/Script.sol";
+import { DN404Factory } from "src/DN404Factory.sol";
+
+struct ScriptData {
+    uint256 chainId;
+    address airlock;
+}
+
+abstract contract DeployTokenFactoryScript is Script {
+    ScriptData internal _scriptData;
+
+    function setUp() public virtual;
+
+    function run() public {
+        vm.startBroadcast();
+        require(block.chainid == _scriptData.chainId, "Invalid chainId");
+        DN404Factory tokenFactory = new DN404Factory(_scriptData.airlock);
+        vm.stopBroadcast();
+    }
+}
+
+// @dev forge script DeployTokenFactoryBaseScript --rpc-url $BASE_MAINNET_RPC_URL --broadcast --verify --slow --private-key $PRIVATE_KEY
+contract DeployTokenFactoryBaseScript is DeployTokenFactoryScript {
+    function setUp() public override {
+        _scriptData = ScriptData({ chainId: 8453, airlock: 0x660eAaEdEBc968f8f3694354FA8EC0b4c5Ba8D12 });
+    }
+}
+
+// @dev forge script DeployTokenFactoryBaseSepoliaScript --rpc-url $BASE_SEPOLIA_RPC_URL --broadcast --verify --slow --private-key $PRIVATE_KEY
+contract DeployTokenFactoryBaseSepoliaScript is DeployTokenFactoryScript {
+    function setUp() public override {
+        _scriptData = ScriptData({ chainId: 84_532, airlock: 0x3411306Ce66c9469BFF1535BA955503c4Bde1C6e });
+    }
+}
