afe043177e732d1c732752c7f5e40a2322acaac7
clemlak <39790678+clemlak@users.noreply.github.com>
2025-08-25 17:20:22 +0400

    test: add MiniV4ManagerTest test file and constructor test

---
+++ test/unit/MiniV4ManagerTest.t.sol
@@ -0,0 +1,26 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity ^0.8.13;
+
+import { Test } from "forge-std/Test.sol";
+import { Deployers } from "@uniswap/v4-core/test/utils/Deployers.sol";
+import { IPoolManager, PoolKey, IHooks, BalanceDelta } from "@v4-core/interfaces/IPoolManager.sol";
+import { MiniV4Manager } from "src/MiniV4Manager.sol";
+
+contract MiniV4ManagerImplementation is MiniV4Manager {
+    constructor(
+        IPoolManager poolManager_
+    ) MiniV4Manager(poolManager_) { }
+}
+
+contract MiniV4ManagerTest is Test, Deployers {
+    MiniV4Manager public mini;
+
+    function setUp() public {
+        deployFreshManager();
+        mini = new MiniV4ManagerImplementation(manager);
+    }
+
+    function test_constructor() public view {
+        assertEq(address(mini.poolManager()), address(manager), "Wrong PoolManager address");
+    }
+}
