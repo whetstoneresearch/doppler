98408ee9fa096c98fa7d33f1ddfc6a218c8de0a9
clemlak <39790678+clemlak@users.noreply.github.com>
2025-08-26 08:27:14 +0400

    test: update setUp function in UniswapV4MulticurveInitializerTest

--- test/unit/UniswapV4MulticurveInitializer.t.sol
+++ test/unit/UniswapV4MulticurveInitializer.t.sol
@@ -3,14 +3,18 @@ pragma solidity ^0.8.13;
 
 import { Deployers } from "@uniswap/v4-core/test/utils/Deployers.sol";
 import { IPoolManager, PoolKey, IHooks, BalanceDelta } from "@v4-core/interfaces/IPoolManager.sol";
+import { Hooks } from "@v4-core/libraries/Hooks.sol";
 import { UniswapV4MulticurveInitializer } from "src/UniswapV4MulticurveInitializer.sol";
+import { UniswapV4MulticurveInitializerHook } from "src/UniswapV4MulticurveInitializerHook.sol";
 
 contract UniswapV4MulticurveInitializerTest is Deployers {
     UniswapV4MulticurveInitializer public initializer;
+    UniswapV4MulticurveInitializerHook public hook;
     address public airlock = makeAddr("airlock");
 
     function test_setUp() public {
         deployFreshManager();
-        // initializer = new UniswapV4MulticurveInitializer(airlock, manager);
+        hook = UniswapV4MulticurveInitializerHook(address(uint160(Hooks.BEFORE_ADD_LIQUIDITY_FLAG) ^ (0x4444 << 144)));
+        initializer = new UniswapV4MulticurveInitializer(airlock, manager, hook);
     }
 }
