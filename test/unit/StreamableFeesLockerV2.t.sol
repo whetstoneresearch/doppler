535607a0111f1c8036e2dfbcdb26aec51d30c54b
clemlak <39790678+clemlak@users.noreply.github.com>
2025-08-29 20:57:55 +0400

    test: add StreamableFeesLockerV2 test file

---
+++ test/unit/StreamableFeesLockerV2.t.sol
@@ -0,0 +1,8 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity ^0.8.13;
+
+import { Test } from "forge-std/Test.sol";
+
+import { StreamableFeesLockerV2 } from "src/StreamableFeesLockerV2.sol";
+
+contract StreamableFeesLockerV2Test is Test { }
