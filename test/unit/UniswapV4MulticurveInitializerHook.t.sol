fa88f094df41a4b5796ba218cf4ed7eb49ac3e87
clemlak <39790678+clemlak@users.noreply.github.com>
2025-08-29 11:13:51 +0400

    test: add UniswapV4MulticurveInitializerHookTest

---
+++ test/unit/UniswapV4MulticurveInitializerHook.t.sol
@@ -0,0 +1,29 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity ^0.8.13;
+
+import { Test } from "forge-std/Test.sol";
+
+import { Hooks } from "@v4-core/libraries/Hooks.sol";
+import { PoolKey } from "@v4-core/types/PoolKey.sol";
+import { IPoolManager } from "@v4-core/interfaces/IPoolManager.sol";
+import { ImmutableState } from "@v4-periphery/base/ImmutableState.sol";
+
+import { UniswapV4MulticurveInitializerHook } from "src/UniswapV4MulticurveInitializerHook.sol";
+
+contract UniswapV4MulticurveInitializerHookTest is Test {
+    UniswapV4MulticurveInitializerHook public hook;
+    address poolManager = makeAddr("PoolManager");
+    address migrator = makeAddr("Migrator");
+
+    function setUp() public {
+        hook = UniswapV4MulticurveInitializerHook(address(uint160(Hooks.BEFORE_ADD_LIQUIDITY_FLAG) ^ (0x4444 << 144)));
+        deployCodeTo("UniswapV4MulticurveInitializerHook", abi.encode(poolManager, migrator), address(hook));
+    }
+
+    function test_beforeAddLiquidity_RevertsWhenSenderNotPoolManager() public {
+        PoolKey memory emptyPoolKey;
+        IPoolManager.ModifyLiquidityParams memory emptyParams;
+        vm.expectRevert(ImmutableState.NotPoolManager.selector);
+        hook.beforeAddLiquidity(address(0), emptyPoolKey, emptyParams, new bytes(0));
+    }
+}
