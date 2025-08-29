44d0e24fe949932b5d43cef5481768f130cf09e2
clemlak <39790678+clemlak@users.noreply.github.com>
2025-08-29 11:17:10 +0400

    test: add more tests to UniswapV4MulticurveInitializerHookTest

--- test/unit/UniswapV4MulticurveInitializerHook.t.sol
+++ test/unit/UniswapV4MulticurveInitializerHook.t.sol
@@ -8,22 +8,34 @@ import { PoolKey } from "@v4-core/types/PoolKey.sol";
 import { IPoolManager } from "@v4-core/interfaces/IPoolManager.sol";
 import { ImmutableState } from "@v4-periphery/base/ImmutableState.sol";
 
-import { UniswapV4MulticurveInitializerHook } from "src/UniswapV4MulticurveInitializerHook.sol";
+import { UniswapV4MulticurveInitializerHook, OnlyInitializer } from "src/UniswapV4MulticurveInitializerHook.sol";
 
 contract UniswapV4MulticurveInitializerHookTest is Test {
     UniswapV4MulticurveInitializerHook public hook;
     address poolManager = makeAddr("PoolManager");
-    address migrator = makeAddr("Migrator");
+    address initializer = makeAddr("Migrator");
+
+    PoolKey internal emptyPoolKey;
+    IPoolManager.ModifyLiquidityParams internal emptyParams;
 
     function setUp() public {
         hook = UniswapV4MulticurveInitializerHook(address(uint160(Hooks.BEFORE_ADD_LIQUIDITY_FLAG) ^ (0x4444 << 144)));
-        deployCodeTo("UniswapV4MulticurveInitializerHook", abi.encode(poolManager, migrator), address(hook));
+        deployCodeTo("UniswapV4MulticurveInitializerHook", abi.encode(poolManager, initializer), address(hook));
     }
 
-    function test_beforeAddLiquidity_RevertsWhenSenderNotPoolManager() public {
-        PoolKey memory emptyPoolKey;
-        IPoolManager.ModifyLiquidityParams memory emptyParams;
+    function test_beforeAddLiquidity_RevertsWhenMsgSenderNotPoolManager() public {
         vm.expectRevert(ImmutableState.NotPoolManager.selector);
         hook.beforeAddLiquidity(address(0), emptyPoolKey, emptyParams, new bytes(0));
+    }
+
+    function test_beforeAddLiquidity_RevertsWhenSenderParamNotInitializer() public {
+        vm.prank(poolManager);
+        vm.expectRevert(OnlyInitializer.selector);
+        hook.beforeAddLiquidity(address(0), emptyPoolKey, emptyParams, new bytes(0));
+    }
+
+    function test_beforeAddLiquidity_PassesWhenSenderParamInitializer() public {
+        vm.prank(poolManager);
+        hook.beforeAddLiquidity(initializer, emptyPoolKey, emptyParams, new bytes(0));
     }
 }
