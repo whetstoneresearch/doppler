e1cbfcacd00a5c04ba21a2af9226f67f37863d41
z80 <z80@ophy.xyz>
2025-09-02 13:49:28 -0400

    feat: expose tokenOfOwnerByIndex via Mirror

--- src/dn404/DopplerDN404Mirror.sol
+++ src/dn404/DopplerDN404Mirror.sol
@@ -2,6 +2,7 @@
 pragma solidity ^0.8.24;
 
 import { DN404Mirror } from "lib/dn404/src/DN404Mirror.sol";
+import { DopplerDN404 } from "src/dn404/DopplerDN404.sol";
 
 /// @title DopplerDN404Mirror
 /// @notice Thin wrapper around DN404Mirror for Doppler DN404 deployments.
@@ -9,4 +10,9 @@ contract DopplerDN404Mirror is DN404Mirror {
     constructor(
         address deployer
     ) DN404Mirror(deployer) { }
+
+    function tokenOfOwnerByIndex(address owner, uint256 index) external view returns (uint256) {
+        address base = baseERC20();
+        return DopplerDN404(base).tokenOfOwnerByIndex(owner, index);
+    }
 }
