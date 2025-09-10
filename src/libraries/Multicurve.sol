121917fdef1fc5bfb9c95c8b9f022437b47cbbf2
clemlak <39790678+clemlak@users.noreply.github.com>
2025-09-10 17:54:43 +0400

    chore: remove unused PoolKey import

--- src/libraries/Multicurve.sol
+++ src/libraries/Multicurve.sol
@@ -1,7 +1,6 @@
 // SPDX-License-Identifier: BUSL-1.1
 pragma solidity ^0.8.13;
 
-import { PoolKey } from "@v4-core/types/PoolKey.sol";
 import { FullMath } from "@v4-core/libraries/FullMath.sol";
 import { TickMath } from "@v4-core/libraries/TickMath.sol";
 import { LiquidityAmounts } from "@v4-periphery/libraries/LiquidityAmounts.sol";
