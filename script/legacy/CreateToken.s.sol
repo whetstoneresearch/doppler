c4d141c648a0fe2ad17894a0df0b4678fa00ee2b
clemlak <39790678+clemlak@users.noreply.github.com>
2025-09-10 17:53:09 +0400

    chore: remove unused import

--- script/legacy/CreateToken.s.sol
+++ script/legacy/CreateToken.s.sol
@@ -2,7 +2,6 @@
 pragma solidity ^0.8.24;
 
 import { Script, console } from "forge-std/Script.sol";
-import { IUniswapV2Router02 } from "@uniswap/v2-core/contracts/interfaces/IUniswapV2Router02.sol";
 import {
     Airlock,
     ModuleState,
@@ -14,7 +13,6 @@ import {
 } from "src/Airlock.sol";
 import { TokenFactory } from "src/TokenFactory.sol";
 import { GovernanceFactory } from "src/GovernanceFactory.sol";
-import { IUniswapV2Router02 } from "src/UniswapV2Migrator.sol";
 import { InitData } from "src/UniswapV3Initializer.sol";
 import { DERC20 } from "src/DERC20.sol";
 
