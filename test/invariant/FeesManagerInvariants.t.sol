e021b2bf840b893bed466ed3533f415968e5b5b3
clemlak <39790678+clemlak@users.noreply.github.com>
2025-09-19 15:35:56 +0400

    test: improve FeesManager invariant tests

--- test/invariant/FeesManagerInvariants.t.sol
+++ test/invariant/FeesManagerInvariants.t.sol
@@ -24,15 +24,15 @@ contract FeesManagerHandler is Test {
     mapping(PoolId poolId => mapping(address beneficiary => uint256 owed1)) public ghost_owed1;
     mapping(PoolId poolId => mapping(address beneficiary => uint256 shares)) public ghost_shares;
 
-    mapping(address beneficiary => uint256 balance0) public ghost_balance0;
-    mapping(address beneficiary => uint256 balance1) public ghost_balance1;
-
     mapping(PoolId poolId => uint256 totalOwed0) public ghost_totalOwed0;
     mapping(PoolId poolId => uint256 totalOwed1) public ghost_totalOwed1;
     mapping(PoolId poolId => uint256 totalFees0) public ghost_totalFees0;
     mapping(PoolId poolId => uint256 totalFees1) public ghost_totalFees1;
     mapping(PoolId poolId => uint256 totalClaimed0) public ghost_totalClaimed0;
     mapping(PoolId poolId => uint256 totalClaimed1) public ghost_totalClaimed1;
+
+    mapping(Currency currency => mapping(address beneficiary => uint256 balance)) public ghost_balanceOf;
+    mapping(Currency currency => uint256 owed) public ghost_balanceOwed;
 
     mapping(PoolId poolId => BeneficiaryData[] beneficiaries) internal _beneficiaries;
 
@@ -44,8 +44,18 @@ contract FeesManagerHandler is Test {
         poolManager = poolManager_;
     }
 
+    function entrypoint(uint256 fees0, uint256 fees1) public {
+        // 2% of the time we store new beneficiaries, 98% of the time we collect fees
+        if (fees0 % 100 < 2) {
+            return storeBeneficiaries(fees0);
+        }
+
+        return collectFees(fees0, fees1);
+    }
+
+    /// @dev Cannot let the fuzzer pass random beneficiaries as it'll be too
     function storeBeneficiaries(
-        BeneficiaryData[] memory beneficiaries
+        uint256 seed
     ) public {
         TestERC20 token0 = new TestERC20(0);
         TestERC20 token1 = new TestERC20(0);
@@ -60,6 +70,8 @@ contract FeesManagerHandler is Test {
 
         PoolId poolId = poolKey.toId();
         _poolKeys[poolId] = poolKey;
+
+        BeneficiaryData[] memory beneficiaries = generateBeneficiaries(seed);
 
         for (uint256 i; i != beneficiaries.length; ++i) {
             BeneficiaryData memory beneficiary = beneficiaries[i];
@@ -82,6 +94,10 @@ contract FeesManagerHandler is Test {
         ghost_totalOwed0[poolId] += fees0;
         ghost_totalOwed1[poolId] += fees1;
 
+        // We track the balanced owed for each currency this way to avoid looping through all the pools
+        ghost_balanceOwed[_poolKeys[poolId].currency0] += fees0;
+        ghost_balanceOwed[_poolKeys[poolId].currency1] += fees1;
+
         // Dumbest and most straightforward way of tracking fees to compare against the implementation
         for (uint256 i; i != _beneficiaries[poolId].length; ++i) {
             BeneficiaryData memory beneficiaryData = _beneficiaries[poolId][i];
@@ -98,13 +114,15 @@ contract FeesManagerHandler is Test {
             ghost_owed0[poolId][msg.sender] = 0;
             ghost_owed1[poolId][msg.sender] = 0;
 
-            ghost_balance0[msg.sender] += owed0;
-            ghost_balance1[msg.sender] += owed1;
+            ghost_balanceOf[_poolKeys[poolId].currency0][msg.sender] += owed0;
+            ghost_balanceOf[_poolKeys[poolId].currency1][msg.sender] += owed1;
 
             ghost_totalClaimed0[poolId] += owed0;
             ghost_totalClaimed1[poolId] += owed1;
             ghost_totalOwed0[poolId] -= owed0;
             ghost_totalOwed1[poolId] -= owed1;
+            ghost_balanceOwed[_poolKeys[poolId].currency0] -= owed0;
+            ghost_balanceOwed[_poolKeys[poolId].currency1] -= owed1;
         }
 
         vm.prank(msg.sender);
@@ -147,6 +165,27 @@ contract FeesManagerHandler is Test {
     ) public view returns (BeneficiaryData[] memory) {
         return _beneficiaries[poolId_];
     }
+
+    /// @dev We generate an array of beneficiaries ourselves to make sure its valid
+    function generateBeneficiaries(
+        uint256 seed
+    ) private pure returns (BeneficiaryData[] memory beneficiaries) {
+        uint256 length = seed % 50 + 1;
+        beneficiaries = new BeneficiaryData[](length);
+
+        uint256 S = length * (length + 1) / 2;
+        uint96 totalShares;
+
+        for (uint256 i = 1; i != beneficiaries.length; ++i) {
+            uint96 shares = uint96((WAD - MIN_PROTOCOL_OWNER_SHARES) * i / S);
+            totalShares += shares;
+
+            beneficiaries[i] =
+                BeneficiaryData({ beneficiary: address(uint160(uint160(PROTOCOL_OWNER) + i)), shares: shares });
+        }
+
+        beneficiaries[0] = BeneficiaryData({ beneficiary: PROTOCOL_OWNER, shares: uint96(WAD - totalShares) });
+    }
 }
 
 contract FeesManagerInvariants is Test {
@@ -158,23 +197,16 @@ contract FeesManagerInvariants is Test {
         poolManager = new PoolManagerMock();
         implementation = new FeesManagerImplementation(PROTOCOL_OWNER, poolManager);
 
-        BeneficiaryData[] memory beneficiaries = new BeneficiaryData[](2);
-        beneficiaries[0] = BeneficiaryData({ beneficiary: address(0xaaa), shares: 0.95e18 });
-        beneficiaries[1] = BeneficiaryData({ beneficiary: PROTOCOL_OWNER, shares: 0.05e18 });
-
         handler = new FeesManagerHandler(implementation, poolManager);
 
-        // TODO: For now we're only testing one pool, but we should test multiple pools
-        handler.storeBeneficiaries(beneficiaries);
-
         bytes4[] memory selectors = new bytes4[](1);
-        selectors[0] = handler.collectFees.selector;
-        // TODO: Support `updateBeneficiary`, kind of tricky to implement because we might have to
-        // also track previous beneficiaries and their shares
-        // selectors[1] = handler.updateBeneficiary.selector;
+        selectors[0] = handler.entrypoint.selector;
 
         targetSelector(FuzzSelector({ addr: address(handler), selectors: selectors }));
         targetContract(address(handler));
+
+        // Let's always start with at least one pool with beneficiaries
+        handler.storeBeneficiaries(block.timestamp);
     }
 
     function invariant_OwedFeesMatchBalances() public view {
@@ -185,12 +217,12 @@ contract FeesManagerInvariants is Test {
 
             assertLe(
                 poolKey.currency0.balanceOf(address(implementation)),
-                handler.ghost_totalOwed0(poolIds[i]),
+                handler.ghost_balanceOwed(poolKey.currency0),
                 "Total owed0 should match token0 balance"
             );
             assertLe(
                 poolKey.currency1.balanceOf(address(implementation)),
-                handler.ghost_totalOwed1(poolIds[i]),
+                handler.ghost_balanceOwed(poolKey.currency1),
                 "Total owed1 should match token1 balance"
             );
         }
@@ -209,13 +241,13 @@ contract FeesManagerInvariants is Test {
 
                 assertApproxEqRel(
                     poolKey.currency0.balanceOf(beneficiary.beneficiary),
-                    handler.ghost_balance0(beneficiary.beneficiary),
+                    handler.ghost_balanceOf(poolKey.currency0, beneficiary.beneficiary),
                     0.0001e18,
                     "Claimed0 should match token0 balance"
                 );
                 assertApproxEqRel(
                     poolKey.currency1.balanceOf(beneficiary.beneficiary),
-                    handler.ghost_balance1(beneficiary.beneficiary),
+                    handler.ghost_balanceOf(poolKey.currency1, beneficiary.beneficiary),
                     0.0001e18,
                     "Claimed1 should match token1 balance"
                 );
