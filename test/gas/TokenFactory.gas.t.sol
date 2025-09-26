f0ab52b5854bb65a11c1c097ca4eab3f1377fb76
clemlak <39790678+clemlak@users.noreply.github.com>
2025-09-26 14:37:48 +0400

    test: add TokenFactoryGasTest

---
+++ test/gas/TokenFactory.gas.t.sol
@@ -0,0 +1,36 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity ^0.8.13;
+
+import { AirlockBaseTest } from "test/shared/AirlockBaseTest.sol";
+import { TokenFactory } from "src/TokenFactory.sol";
+import { CloneERC20VotesFactory } from "src/CloneERC20VotesFactory.sol";
+
+contract TokenFactoryGasTest is AirlockBaseTest {
+    function test_gas_TokenFactory_NoRecipients() public {
+        TokenFactory factory = new TokenFactory(address(this));
+
+        vm.startSnapshotGas("TokenFactory", "TokenFactory/NoRecipients");
+        factory.create(
+            1e30,
+            address(this),
+            address(this),
+            bytes32(0),
+            abi.encode("", "", 0, 0, new address[](0), new uint256[](0), "")
+        );
+        vm.stopSnapshotGas("TokenFactory", "TokenFactory/NoRecipients");
+    }
+
+    function test_gas_CloneERC20VotesFactory_NoRecipients() public {
+        CloneERC20VotesFactory factory = new CloneERC20VotesFactory(address(this));
+
+        vm.startSnapshotGas("TokenFactory", "CloneERC20VotesFactory/NoRecipients");
+        factory.create(
+            1e30,
+            address(this),
+            address(this),
+            bytes32(0),
+            abi.encode("", "", 0, 0, new address[](0), new uint256[](0), "")
+        );
+        vm.stopSnapshotGas("TokenFactory", "CloneERC20VotesFactory/NoRecipients");
+    }
+}
