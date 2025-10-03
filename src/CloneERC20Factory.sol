cd78c9db7129b99c86fed170b09ae96b2e589fcc
clemlak <39790678+clemlak@users.noreply.github.com>
2025-10-03 18:30:38 +0400

    chore: add NatSpec

--- src/CloneERC20Factory.sol
+++ src/CloneERC20Factory.sol
@@ -6,9 +6,17 @@ import { ImmutableAirlock } from "src/base/ImmutableAirlock.sol";
 import { ITokenFactory } from "src/interfaces/ITokenFactory.sol";
 import { CloneERC20 } from "src/CloneERC20.sol";
 
+/**
+ * @title CloneERC20Factory
+ * @author Whetstone Research
+ * @notice Deploys new ERC20 tokens using the minimal proxy pattern (EIP-1167).
+ * @custom:security-contact security@whetstone.cc
+ */
 contract CloneERC20Factory is ImmutableAirlock, ITokenFactory {
+    /// @notice Address of the implementation contract which will be cloned
     address public immutable IMPLEMENTATION;
 
+    /// @notice airlock_ Address of the Airlock contract
     constructor(
         address airlock_
     ) ImmutableAirlock(airlock_) {
@@ -18,6 +26,24 @@ contract CloneERC20Factory is ImmutableAirlock, ITokenFactory {
         );
     }
 
+    /**
+     * @notice Deploys a new ERC20 token
+     * @dev This function (only callable by the Airlock) clones the implementation contract
+     * and initializes it with the provided parameters
+     * @param initialSupply Initial supply of the token
+     * @param recipient Address to receive the initial supply
+     * @param owner Address receiving owner privileges
+     * @param salt Salt for deterministic deployment
+     * @param tokenData Encoded token parameters:
+     * - name: Name of the token
+     * - symbol: Symbol of the token
+     * - yearlyMintRate (optional): Yearly mint rate (in WAD)
+     * - recipients (optional): Addresses to receive vested tokens
+     * - amounts (optional): Amounts to be vested to each recipient
+     * - vestingDuration (optional): Duration of the vesting period in seconds
+     * - tokenURI (optional): Token URI for metadata
+     * @return asset Address of the newly deployed token
+     */
     function create(
         uint256 initialSupply,
         address recipient,
