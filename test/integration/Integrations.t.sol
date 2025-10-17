61f5b5c70bfd271b98cc1979fd26eb878c2babec
clemlak <39790678+clemlak@users.noreply.github.com>
2025-10-17 14:57:17 +0400

    test: add CloneERC20FactoryUniswapV4InitializerNoOpGovernanceFactoryNoOpMigratorIntegrationTest

--- test/integration/Integrations.t.sol
+++ test/integration/Integrations.t.sol
@@ -12,14 +12,51 @@ import { NoOpGovernanceFactory } from "src/NoOpGovernanceFactory.sol";
 import { NoOpMigrator } from "src/NoOpMigrator.sol";
 import { DopplerDeployer, UniswapV4Initializer } from "src/UniswapV4Initializer.sol";
 import { deployUniswapV4Initializer, preparePoolInitializerData } from "test/integration/UniswapV4Initializer.t.sol";
+import { deployCloneERC20Factory } from "test/integration/CloneERC20Factory.t.sol";
+import { CloneERC20Factory } from "src/CloneERC20Factory.sol";
 
-contract TokenFactoryUniswapV4InitializerNoOpMigratorNoOpGovernanceFactory is BaseIntegrationTest {
+contract TokenFactoryUniswapV4InitializerNoOpGovernanceFactoryNoOpMigratorIntegrationTest is BaseIntegrationTest {
     function setUp() public override {
         super.setUp();
 
-        name = "TokenFactory + UniswapV4Initializer + NoOpMigrator + NoOpGovernanceFactory";
+        name = "TokenFactoryUniswapV4InitializerNoOpGovernanceFactoryNoOpMigrator";
 
         TokenFactory tokenFactory = deployTokenFactory(vm, airlock, AIRLOCK_OWNER);
+        createParams.tokenFactory = tokenFactory;
+        createParams.tokenFactoryData =
+            abi.encode("Test Token", "TEST", 0, 0, new address[](0), new uint256[](0), "TOKEN_URI");
+
+        (, UniswapV4Initializer initializer) = deployUniswapV4Initializer(vm, airlock, AIRLOCK_OWNER, address(manager));
+        createParams.poolInitializer = initializer;
+        (bytes32 salt, bytes memory poolInitializerData) = preparePoolInitializerData(
+            address(airlock),
+            address(manager),
+            address(tokenFactory),
+            createParams.tokenFactoryData,
+            address(initializer)
+        );
+        createParams.poolInitializerData = poolInitializerData;
+        createParams.salt = salt;
+        createParams.numTokensToSell = 1e23;
+        createParams.initialSupply = 1e23;
+
+        NoOpMigrator migrator = deployNoOpMigrator(vm, airlock, AIRLOCK_OWNER);
+        createParams.liquidityMigrator = migrator;
+
+        NoOpGovernanceFactory governanceFactory = deployNoOpGovernanceFactory(vm, airlock, AIRLOCK_OWNER);
+        createParams.governanceFactory = governanceFactory;
+
+        shouldSkipMigrate = true;
+    }
+}
+
+contract CloneERC20FactoryUniswapV4InitializerNoOpGovernanceFactoryNoOpMigratorIntegrationTest is BaseIntegrationTest {
+    function setUp() public override {
+        super.setUp();
+
+        name = "CloneERC20FactoryUniswapV4InitializerNoOpGovernanceFactoryNoOpMigrator";
+
+        CloneERC20Factory tokenFactory = deployCloneERC20Factory(vm, airlock, AIRLOCK_OWNER);
         createParams.tokenFactory = tokenFactory;
         createParams.tokenFactoryData =
             abi.encode("Test Token", "TEST", 0, 0, new address[](0), new uint256[](0), "TOKEN_URI");
