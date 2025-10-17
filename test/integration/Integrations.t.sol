8271dae8ed165e7f649788d908caef0aace700a6
clemlak <39790678+clemlak@users.noreply.github.com>
2025-10-17 19:33:23 +0400

    test: add new integration tests

--- test/integration/Integrations.t.sol
+++ test/integration/Integrations.t.sol
@@ -5,15 +5,26 @@ import {
     BaseIntegrationTest,
     deployTokenFactory,
     deployNoOpMigrator,
-    deployNoOpGovernanceFactory
+    deployNoOpGovernanceFactory,
+    prepareTokenFactoryData,
+    deployGovernanceFactory,
+    prepareGovernanceFactoryData
 } from "test/integration/BaseIntegrationTest.sol";
 import { TokenFactory } from "src/TokenFactory.sol";
 import { NoOpGovernanceFactory } from "src/NoOpGovernanceFactory.sol";
+import { GovernanceFactory } from "src/GovernanceFactory.sol";
 import { NoOpMigrator } from "src/NoOpMigrator.sol";
 import { DopplerDeployer, UniswapV4Initializer } from "src/UniswapV4Initializer.sol";
 import { deployUniswapV4Initializer, preparePoolInitializerData } from "test/integration/UniswapV4Initializer.t.sol";
+import {
+    deployUniswapV4MulticurveInitializer,
+    prepareUniswapV4MulticurveInitializerData
+} from "test/integration/UniswapV4MulticurveInitializer.t.sol";
+import { UniswapV4MulticurveInitializer } from "src/UniswapV4MulticurveInitializer.sol";
 import { deployCloneERC20Factory } from "test/integration/CloneERC20Factory.t.sol";
 import { CloneERC20Factory } from "src/CloneERC20Factory.sol";
+import { deployCloneERC20VotesFactory } from "test/integration/CloneERC20VotesFactory.t.sol";
+import { CloneERC20VotesFactory } from "src/CloneERC20VotesFactory.sol";
 
 contract TokenFactoryUniswapV4InitializerNoOpGovernanceFactoryNoOpMigratorIntegrationTest is BaseIntegrationTest {
     function setUp() public override {
@@ -80,6 +91,69 @@ contract CloneERC20FactoryUniswapV4InitializerNoOpGovernanceFactoryNoOpMigratorI
 
         NoOpGovernanceFactory governanceFactory = deployNoOpGovernanceFactory(vm, airlock, AIRLOCK_OWNER);
         createParams.governanceFactory = governanceFactory;
+
+        shouldSkipMigrate = true;
+    }
+}
+
+contract CloneERC20FactoryUniswapV4MulticurveInitializerNoOpGovernanceFactoryNoOpMigratorIntegrationTest is
+    BaseIntegrationTest
+{
+    function setUp() public override {
+        super.setUp();
+
+        name = "CloneERC20FactoryUniswapV4MulticurveInitializerNoOpGovernanceFactoryNoOpMigrator";
+
+        CloneERC20Factory tokenFactory = deployCloneERC20Factory(vm, airlock, AIRLOCK_OWNER);
+        createParams.tokenFactory = tokenFactory;
+        (, createParams.tokenFactoryData) =
+            prepareTokenFactoryData(vm, address(airlock), address(tokenFactory), bytes32(0));
+
+        (, UniswapV4MulticurveInitializer initializer) =
+            deployUniswapV4MulticurveInitializer(vm, _deployCodeTo, airlock, AIRLOCK_OWNER, address(manager));
+        createParams.poolInitializer = initializer;
+        (bytes memory poolInitializerData) = prepareUniswapV4MulticurveInitializerData(address(0), address(0));
+        createParams.poolInitializerData = poolInitializerData;
+        createParams.numTokensToSell = 1e23;
+        createParams.initialSupply = 1e23;
+
+        NoOpMigrator migrator = deployNoOpMigrator(vm, airlock, AIRLOCK_OWNER);
+        createParams.liquidityMigrator = migrator;
+
+        NoOpGovernanceFactory governanceFactory = deployNoOpGovernanceFactory(vm, airlock, AIRLOCK_OWNER);
+        createParams.governanceFactory = governanceFactory;
+
+        shouldSkipMigrate = true;
+    }
+}
+
+contract CloneVotesERC20FactoryUniswapV4MulticurveInitializerGovernanceFactoryNoOpMigratorIntegrationTest is
+    BaseIntegrationTest
+{
+    function setUp() public override {
+        super.setUp();
+
+        name = "CloneVotesERC20FactoryUniswapV4MulticurveInitializerGovernanceFactoryNoOpMigrator";
+
+        CloneERC20VotesFactory tokenFactory = deployCloneERC20VotesFactory(vm, airlock, AIRLOCK_OWNER);
+        createParams.tokenFactory = tokenFactory;
+        (, createParams.tokenFactoryData) =
+            prepareTokenFactoryData(vm, address(airlock), address(tokenFactory), bytes32(0));
+
+        (, UniswapV4MulticurveInitializer initializer) =
+            deployUniswapV4MulticurveInitializer(vm, _deployCodeTo, airlock, AIRLOCK_OWNER, address(manager));
+        createParams.poolInitializer = initializer;
+        (bytes memory poolInitializerData) = prepareUniswapV4MulticurveInitializerData(address(0), address(0));
+        createParams.poolInitializerData = poolInitializerData;
+        createParams.numTokensToSell = 1e23;
+        createParams.initialSupply = 1e23;
+
+        NoOpMigrator migrator = deployNoOpMigrator(vm, airlock, AIRLOCK_OWNER);
+        createParams.liquidityMigrator = migrator;
+
+        GovernanceFactory governanceFactory = deployGovernanceFactory(vm, airlock, AIRLOCK_OWNER);
+        createParams.governanceFactory = governanceFactory;
+        createParams.governanceFactoryData = prepareGovernanceFactoryData();
 
         shouldSkipMigrate = true;
     }
