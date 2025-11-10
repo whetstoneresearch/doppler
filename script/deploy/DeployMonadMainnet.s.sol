80e95def723c1648e3d80fd297390718dbbec544
clemlak <39790678+clemlak@users.noreply.github.com>
2025-11-10 17:30:08 +0400

    build: add deployment scripts for Monad

--- script/deploy/DeployMonadMainnet.s.sol
+++ script/deploy/DeployMonadMainnet.s.sol
@@ -14,12 +14,13 @@ import { UniswapV3Initializer, IUniswapV3Factory } from "src/UniswapV3Initialize
 import { Bundler } from "src/Bundler.sol";
 import { LockableUniswapV3Initializer } from "src/LockableUniswapV3Initializer.sol";
 import { NoOpGovernanceFactory } from "src/NoOpGovernanceFactory.sol";
-import { DookMulticurveInitializer } from "src/DookMulticurveInitializer.sol";
 import { NoOpMigrator } from "src/NoOpMigrator.sol";
 import { AirlockMultisig } from "test/shared/AirlockMultisig.sol";
+import { LaunchpadGovernanceFactory } from "src/LaunchpadGovernanceFactory.sol";
+import { DookMulticurveInitializer } from "src/DookMulticurveInitializer.sol";
 import { mineDookMulticurveInitializer, MineDookMulticurveInitializerParams } from "test/shared/AirlockMiner.sol";
 
-contract DeployMonadTestnetScript is Script {
+contract DeployMonadMainnetScript is Script {
     function run() public {
         console.log(unicode"🚀 Deploying on chain %s with sender %s...", vm.toString(block.chainid), msg.sender);
 
@@ -31,6 +32,7 @@ contract DeployMonadTestnetScript is Script {
         TokenFactory tokenFactory = new TokenFactory(airlock);
         NoOpGovernanceFactory noOpGovernanceFactory = new NoOpGovernanceFactory();
         NoOpMigrator noOpMigrator = new NoOpMigrator(airlock);
+        LaunchpadGovernanceFactory launchpadGovernanceFactory = new LaunchpadGovernanceFactory();
 
         address[] memory signers = new address[](1);
         signers[0] = msg.sender;
@@ -38,20 +40,18 @@ contract DeployMonadTestnetScript is Script {
 
         UniswapV2Migrator uniswapV2LiquidityMigrator = new UniswapV2Migrator(
             airlock,
-            IUniswapV2Factory(0x733E88f248b742db6C14C0b1713Af5AD7fDd59D0),
-            IUniswapV2Router02(0xfB8e1C3b833f9E67a71C859a132cf783b645e436),
+            IUniswapV2Factory(0x182a927119D56008d921126764bF884221b10f59),
+            IUniswapV2Router02(0x4B2ab38DBF28D31D467aA8993f6c2585981D6804),
             address(airlockMultisig)
         );
 
-        UniswapV3Initializer uniswapV3Initializer =
-            new UniswapV3Initializer(address(airlock), IUniswapV3Factory(0x961235a9020B05C44DF1026D956D1F4D78014276));
         LockableUniswapV3Initializer lockableUniswapV3Initializer =
             new LockableUniswapV3Initializer(airlock, IUniswapV3Factory(0x961235a9020B05C44DF1026D956D1F4D78014276));
 
         Bundler bundler = new Bundler(
             Airlock(payable(airlock)),
-            UniversalRouter(payable(0x3aE6D8A282D67893e17AA70ebFFb33EE5aa65893)),
-            IQuoterV2(0x1b4E313fEF15630AF3e6F2dE550Dbf4cC9D3081d)
+            UniversalRouter(payable(0x0D97Dc33264bfC1c226207428A79b26757fb9dc3)),
+            IQuoterV2(0x661E93cca42AfacB172121EF892830cA3b70F08d)
         );
 
         (bytes32 salt, address minedDookMulticurveInitializer) = mineDookMulticurveInitializer(
@@ -70,18 +70,18 @@ contract DeployMonadTestnetScript is Script {
         );
 
         // Whitelisting the initial modules
-        address[] memory modules = new address[](7);
+        address[] memory modules = new address[](6);
         modules[0] = address(tokenFactory);
-        modules[1] = address(uniswapV3Initializer);
+        modules[1] = address(launchpadGovernanceFactory);
         modules[2] = address(governanceFactory);
         modules[3] = address(uniswapV2LiquidityMigrator);
         modules[4] = address(lockableUniswapV3Initializer);
         modules[5] = address(noOpGovernanceFactory);
         modules[6] = address(noOpMigrator);
 
-        ModuleState[] memory states = new ModuleState[](7);
+        ModuleState[] memory states = new ModuleState[](6);
         states[0] = ModuleState.TokenFactory;
-        states[1] = ModuleState.PoolInitializer;
+        states[1] = ModuleState.GovernanceFactory;
         states[2] = ModuleState.GovernanceFactory;
         states[3] = ModuleState.LiquidityMigrator;
         states[4] = ModuleState.PoolInitializer;
