531e22b0a6059cc27d59449340c84f9599595cf2
clemlak <39790678+clemlak@users.noreply.github.com>
2025-11-18 10:23:55 +0400

    test: reorg BaseDookTest

--- test/unit/BaseDook.t.sol
+++ test/unit/BaseDook.t.sol
@@ -3,25 +3,24 @@ pragma solidity ^0.8.13;
 
 import { Test } from "forge-std/Test.sol";
 
+import { IHooks } from "@v4-core/interfaces/IHooks.sol";
 import { IPoolManager } from "@v4-core/interfaces/IPoolManager.sol";
 import { toBalanceDelta } from "@v4-core/types/BalanceDelta.sol";
-import { PoolKey } from "@v4-core/types/PoolKey.sol";
 import { Currency } from "@v4-core/types/Currency.sol";
-import { IHooks } from "@v4-core/interfaces/IHooks.sol";
-import { BaseDook, SenderNotInitializer, SenderNotHook } from "src/base/BaseDook.sol";
+import { PoolKey } from "@v4-core/types/PoolKey.sol";
+import { BaseDook, SenderNotInitializer } from "src/base/BaseDook.sol";
 
 contract DookMock is BaseDook {
     constructor(address initializer) BaseDook(initializer) { }
 }
 
 contract BaseDookTest is Test {
-    DookMock baseDook;
-    PoolKey key;
-
-    address initializer = makeAddr("initializer");
+    BaseDook internal dook;
+    PoolKey internal key;
+    address internal initializer = makeAddr("initializer");
 
     function setUp() public {
-        baseDook = new DookMock(initializer);
+        dook = BaseDook(new DookMock(initializer));
     }
 
     /* --------------------------------------------------------------------------- */
@@ -29,7 +28,7 @@ contract BaseDookTest is Test {
     /* --------------------------------------------------------------------------- */
 
     function test_constructor() public view {
-        assertEq(baseDook.INITIALIZER(), initializer);
+        assertEq(dook.INITIALIZER(), initializer);
     }
 
     /* -------------------------------------------------------------------------------- */
@@ -38,12 +37,12 @@ contract BaseDookTest is Test {
 
     function test_onInitialization_RevertsWhenMsgSenderNotInitializer() public {
         vm.expectRevert(SenderNotInitializer.selector);
-        baseDook.onInitialization(address(0), key, new bytes(0));
+        dook.onInitialization(address(0), key, new bytes(0));
     }
 
     function test_onInitialization_PassesWhenMsgSenderInitializer() public {
         vm.prank(initializer);
-        baseDook.onInitialization(address(0), key, new bytes(0));
+        dook.onInitialization(address(0), key, new bytes(0));
     }
 
     /* ------------------------------------------------------------------------------ */
@@ -52,12 +51,12 @@ contract BaseDookTest is Test {
 
     function test_onGraduation_RevertsWhenMsgSenderNotInitializer() public {
         vm.expectRevert(SenderNotInitializer.selector);
-        baseDook.onGraduation(address(0), key, new bytes(0));
+        dook.onGraduation(address(0), key, new bytes(0));
     }
 
     function test_onGraduation_PassesWhenMsgSenderInitializer() public {
         vm.prank(initializer);
-        baseDook.onGraduation(address(0), key, new bytes(0));
+        dook.onGraduation(address(0), key, new bytes(0));
     }
 
     /* ------------------------------------------------------------------------------ */
@@ -66,7 +65,7 @@ contract BaseDookTest is Test {
 
     function test_onSwap_RevertsWhenMsgSenderNotInitializer() public {
         vm.expectRevert(SenderNotInitializer.selector);
-        baseDook.onSwap(
+        dook.onSwap(
             address(0),
             PoolKey(Currency.wrap(address(0)), Currency.wrap(address(0)), 0, 0, IHooks(address(0))),
             IPoolManager.SwapParams(false, 0, 0),
@@ -77,7 +76,7 @@ contract BaseDookTest is Test {
 
     function test_onSwap_PassesWhenMsgSenderInitializer() public {
         vm.prank(initializer);
-        baseDook.onSwap(
+        dook.onSwap(
             address(0),
             PoolKey(Currency.wrap(address(0)), Currency.wrap(address(0)), 0, 0, IHooks(address(0))),
             IPoolManager.SwapParams(false, 0, 0),
