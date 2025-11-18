bbdf09fc1456d3b638c35c5123dc2f636e3cca5e
clemlak <39790678+clemlak@users.noreply.github.com>
2025-11-18 10:24:41 +0400

    test: add tests for SwapRestrictorDookTest

--- test/unit/dooks/SwapRestrictorDook.t.sol
+++ test/unit/dooks/SwapRestrictorDook.t.sol
@@ -1,15 +1,141 @@
 // SPDX-License-Identifier: UNLICENSED
 pragma solidity ^0.8.13;
 
+import { IPoolManager } from "@v4-core/interfaces/IPoolManager.sol";
+import { BalanceDeltaLibrary, toBalanceDelta } from "@v4-core/types/BalanceDelta.sol";
+import { Currency } from "@v4-core/types/Currency.sol";
+import { PoolId } from "@v4-core/types/PoolId.sol";
+import { PoolKey } from "@v4-core/types/PoolKey.sol";
 import { Test } from "forge-std/Test.sol";
-import { SwapRestrictorDook } from "src/dooks/SwapRestrictorDook.sol";
+import { SenderNotInitializer } from "src/base/BaseDook.sol";
+import { InsufficientAmountLeft, SwapRestrictorDook, UpdatedAmountLeft } from "src/dooks/SwapRestrictorDook.sol";
 
 contract SwapRestrictorDookTest is Test {
-    address initializer = makeAddr("initializer");
-
-    SwapRestrictorDook public dook;
+    SwapRestrictorDook internal dook;
+    address internal initializer = makeAddr("initializer");
 
     function setUp() public {
         dook = new SwapRestrictorDook(initializer);
+    }
+
+    /* -------------------------------------------------------------------------------- */
+    /*                                onInitialization()                                */
+    /* -------------------------------------------------------------------------------- */
+
+    function test_onInitialization_StoresAmountsLeft(
+        bool isTokenZero,
+        PoolKey calldata poolKey,
+        address[] calldata approved,
+        uint256 maxAmount
+    ) public {
+        address asset = Currency.unwrap(isTokenZero ? poolKey.currency0 : poolKey.currency1);
+        PoolId poolId = poolKey.toId();
+
+        for (uint256 i; i < approved.length; i++) {
+            vm.expectEmit();
+            emit UpdatedAmountLeft(poolId, approved[i], maxAmount);
+        }
+
+        vm.prank(initializer);
+        dook.onInitialization(asset, poolKey, abi.encode(approved, maxAmount));
+
+        for (uint256 i; i < approved.length; i++) {
+            assertEq(dook.amountLeftOf(poolId, approved[i]), maxAmount);
+        }
+    }
+
+    /* ---------------------------------------------------------------------- */
+    /*                                onSwap()                                */
+    /* ---------------------------------------------------------------------- */
+
+    function test_onSwap_RevertsWhenSenderNotInitializer(
+        PoolKey calldata poolKey,
+        IPoolManager.SwapParams calldata swapParams
+    ) public {
+        vm.expectRevert(SenderNotInitializer.selector);
+        dook.onSwap(address(0), poolKey, swapParams, BalanceDeltaLibrary.ZERO_DELTA, new bytes(0));
+    }
+
+    function test_onSwap_DecreasesAmountLeftWhenBuyingAsset(bool isTokenZero, PoolKey calldata poolKey) public {
+        address asset = Currency.unwrap(isTokenZero ? poolKey.currency0 : poolKey.currency1);
+        address[] memory approved = new address[](1);
+        approved[0] = address(0x123);
+        uint256 maxAmount = 1e18;
+
+        vm.prank(initializer);
+        dook.onInitialization(asset, poolKey, abi.encode(approved, maxAmount));
+
+        IPoolManager.SwapParams memory swapParams =
+            IPoolManager.SwapParams({ zeroForOne: !isTokenZero, amountSpecified: 0, sqrtPriceLimitX96: 0 });
+
+        vm.expectEmit();
+        emit UpdatedAmountLeft(poolKey.toId(), approved[0], 0);
+
+        vm.prank(initializer);
+        dook.onSwap(
+            approved[0],
+            poolKey,
+            swapParams,
+            toBalanceDelta(isTokenZero ? int128(1e18) : int128(0), isTokenZero ? int128(0) : int128(1e18)),
+            new bytes(0)
+        );
+
+        assertEq(dook.amountLeftOf(poolKey.toId(), approved[0]), 0);
+    }
+
+    function test_onSwap_IgnoresAmountLeftWhenSellingAsset(bool isTokenZero, PoolKey calldata poolKey) public {
+        address asset = Currency.unwrap(isTokenZero ? poolKey.currency0 : poolKey.currency1);
+        address[] memory approved = new address[](1);
+        approved[0] = address(0x123);
+        uint256 maxAmount = 1e18;
+
+        vm.prank(initializer);
+        dook.onInitialization(asset, poolKey, abi.encode(approved, maxAmount));
+
+        IPoolManager.SwapParams memory swapParams =
+            IPoolManager.SwapParams({ zeroForOne: !isTokenZero, amountSpecified: 0, sqrtPriceLimitX96: 0 });
+
+        vm.prank(initializer);
+        dook.onSwap(
+            approved[0],
+            poolKey,
+            swapParams,
+            toBalanceDelta(!isTokenZero ? int128(1e18) : int128(0), !isTokenZero ? int128(0) : int128(1e18)),
+            new bytes(0)
+        );
+
+        assertEq(dook.amountLeftOf(poolKey.toId(), approved[0]), maxAmount);
+    }
+
+    function test_onSwap_RevertsWhenTryingToBuyMoreThanLeftAmount(bool isTokenZero, PoolKey calldata poolKey) public {
+        address asset = Currency.unwrap(isTokenZero ? poolKey.currency0 : poolKey.currency1);
+        address[] memory approved = new address[](1);
+        approved[0] = address(0x123);
+        uint256 maxAmount = 1e18;
+
+        vm.prank(initializer);
+        dook.onInitialization(asset, poolKey, abi.encode(approved, maxAmount));
+
+        IPoolManager.SwapParams memory swapParams =
+            IPoolManager.SwapParams({ zeroForOne: !isTokenZero, amountSpecified: 0, sqrtPriceLimitX96: 0 });
+
+        vm.expectRevert(
+            abi.encodeWithSelector(
+                InsufficientAmountLeft.selector,
+                poolKey.toId(),
+                approved[0],
+                2e18,
+                dook.amountLeftOf(poolKey.toId(), approved[0])
+            )
+        );
+
+        vm.prank(initializer);
+        dook.onSwap(
+            approved[0],
+            poolKey,
+            swapParams,
+            toBalanceDelta(isTokenZero ? int128(2e18) : int128(0), isTokenZero ? int128(0) : int128(2e18)),
+            new bytes(0)
+        );
     }
 }
