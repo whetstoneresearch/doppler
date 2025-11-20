e688b318a2257a491eced2046a676b6b90067936
clemlak <39790678+clemlak@users.noreply.github.com>
2025-11-20 17:23:09 +0400

    build: try out new deployment script using Config

--- script/Deploy.s.sol
+++ script/Deploy.s.sol
@@ -3,6 +3,7 @@ pragma solidity ^0.8.24;
 
 import { Config } from "forge-std/Config.sol";
 import { Script } from "forge-std/Script.sol";
+import { console } from "forge-std/console.sol";
 
 import { NoOpGovernanceFactory } from "src/modules/governance/NoOpGovernanceFactory.sol";
 import {
@@ -10,19 +11,32 @@ import {
 } from "src/modules/initializers/UniswapV4ScheduledMulticurveInitializer.sol";
 import { NoOpMigrator } from "src/modules/migrators/NoOpMigrator.sol";
 
-contract DeployScript is Script, Config {
+contract DeployScript2 is Script, Config {
     NoOpMigrator public noOpMigrator;
     NoOpGovernanceFactory public noOpGovernanceFactory;
     UniswapV4ScheduledMulticurveInitializer public uniswapV4ScheduledMulticurveInitializer;
 
     function run() public {
-        _loadConfig("./deployments.toml", true);
+        _loadConfigAndForks("./deployments.toml", true);
 
-        uint256 chainId = block.chainid;
-        console.log("Deploying to chainId:", chainId);
+        for (uint256 i; i < chainIds.length; i++) {
+            console.log("Loaded chain %s", chainIds[i]);
+        }
 
         vm.startBroadcast();
 
+        // TODO: Deploy all of our contracts here
+
+        // _checkAndDeploy("pool_manager");
+
         vm.stopBroadcast();
+    }
+
+    function _checkAndDeploy(string memory contractName) internal returns (address deployedAddress) {
+        deployedAddress = config.get(contractName).toAddress();
+
+        if (deployedAddress == address(0)) {
+            console.log("Deploying %s...", contractName);
+        }
     }
 }
