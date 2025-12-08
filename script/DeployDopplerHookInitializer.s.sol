81ac8be3c8dc6c02a1051d5cb0de2264ebce694a
clemlak <39790678+clemlak@users.noreply.github.com>
2025-12-08 12:11:29 +0100

    build: update DopplerHookInitializer deployment script to use create3

--- script/DeployDopplerHookInitializer.s.sol
+++ script/DeployDopplerHookInitializer.s.sol
@@ -4,23 +4,42 @@ pragma solidity ^0.8.13;
 import { IPoolManager } from "@v4-core/interfaces/IPoolManager.sol";
 import { Config } from "forge-std/Config.sol";
 import { Script } from "forge-std/Script.sol";
+import { console } from "forge-std/console.sol";
+import { ICreateX } from "script/ICreateX.sol";
 import { DopplerHookInitializer } from "src/initializers/DopplerHookInitializer.sol";
 import { MineDopplerHookInitializerParams, mineDopplerHookInitializer } from "test/shared/AirlockMiner.sol";
 
 contract DeployDopplerHookInitializerScript is Script, Config {
     function run() public {
-        _loadConfig("./deployments.config.toml", true);
+        _loadConfigAndForks("./deployments.config.toml", true);
+
+        for (uint256 i; i < chainIds.length; i++) {
+            uint256 chainId = chainIds[i];
+            deployToChain(chainId);
+        }
+    }
+
+    function deployToChain(uint256 chainId) internal {
+        vm.selectFork(forkOf[chainId]);
 
         address airlock = config.get("airlock").toAddress();
+        address createX = config.get("create_x").toAddress();
         address poolManager = config.get("uniswap_v4_pool_manager").toAddress();
 
         (bytes32 salt, address deployedTo) = mineDopplerHookInitializer(
-            MineDopplerHookInitializerParams({ airlock: airlock, poolManager: poolManager, deployer: msg.sender })
+            MineDopplerHookInitializerParams({
+                airlock: airlock, poolManager: poolManager, sender: msg.sender, deployer: createX
+            })
         );
 
         vm.startBroadcast();
-        DopplerHookInitializer dopplerHookInitializer =
-            new DopplerHookInitializer{ salt: salt }(airlock, IPoolManager(poolManager));
+        address dopplerHookInitializer = ICreateX(createX)
+            .deployCreate3(
+                salt, abi.encodePacked(type(DopplerHookInitializer).creationCode, abi.encode(airlock, poolManager))
+            );
+
+        console.log("DopplerHookInitializer deployed to:", dopplerHookInitializer);
+        // config.set("doppler_hook_initializer", dopplerHookInitializer);
         vm.stopBroadcast();
     }
 }
