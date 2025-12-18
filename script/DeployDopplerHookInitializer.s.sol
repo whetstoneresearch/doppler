ace309af192d990f05465aedba860c1402b4a669
clemlak <39790678+clemlak@users.noreply.github.com>
2025-12-18 10:32:33 +0100

    build: add ComputeDopplerHookInitializerSaltScript

--- script/DeployDopplerHookInitializer.s.sol
+++ script/DeployDopplerHookInitializer.s.sol
@@ -9,6 +9,21 @@ import { ICreateX } from "script/ICreateX.sol";
 import { DopplerHookInitializer } from "src/initializers/DopplerHookInitializer.sol";
 import { MineDopplerHookInitializerParams, mineDopplerHookInitializer } from "test/shared/AirlockMiner.sol";
 
+contract ComputeDopplerHookInitializerSaltScript is Script, Config {
+    function run() public {
+        address airlock = address(0xbeef);
+        address createX = 0xba5Ed099633D3B313e4D5F7bdc1305d3c28ba5Ed;
+        address poolManager = address(0xbaaf);
+        address sender = address(0xaCE07c3c1D3b556D42633211f0Da71dc6F6d1c42);
+
+        (bytes32 salt, address deployedTo) =
+            mineDopplerHookInitializer(MineDopplerHookInitializerParams({ sender: sender, deployer: createX }));
+
+        console.log("Computed salt:");
+        console.logBytes32(salt);
+    }
+}
+
 contract DeployDopplerHookInitializerScript is Script, Config {
     function run() public {
         _loadConfigAndForks("./deployments.config.toml", true);
@@ -26,11 +41,7 @@ contract DeployDopplerHookInitializerScript is Script, Config {
         address createX = config.get("create_x").toAddress();
         address poolManager = config.get("uniswap_v4_pool_manager").toAddress();
 
-        (bytes32 salt, address deployedTo) = mineDopplerHookInitializer(
-            MineDopplerHookInitializerParams({
-                airlock: airlock, poolManager: poolManager, sender: msg.sender, deployer: createX
-            })
-        );
+        bytes32 salt = 0xace07c3c1d3b556d42633211f0da71dc6f6d1c420000000000000000000014dd;
 
         vm.startBroadcast();
         address dopplerHookInitializer = ICreateX(createX)
