9e6ca334705bab2152474219a07c3184ffc617cb
matt <matt.czernik@gmail.com>
2025-12-31 13:21:51 -0500

    feat: add dopplerHookInitializer support to airlock multisig

--- script/DeployAirlockMultisig.s.sol
+++ script/DeployAirlockMultisig.s.sol
@@ -5,10 +5,12 @@ import { Script } from "forge-std/Script.sol";
 import { ChainIds } from "script/ChainIds.sol";
 import { Airlock } from "src/Airlock.sol";
 import { AirlockMultisig } from "test/shared/AirlockMultisig.sol";
+import { DopplerHookInitializer } from "src/initializers/DopplerHookInitializer.sol";
 
 struct ScriptData {
     uint256 chainId;
     address airlock;
+    address dopplerHookInitializer;
     address[] signers;
 }
 
@@ -20,7 +22,7 @@ abstract contract DeployAirlockMultisigScript is Script {
     function run() public {
         vm.startBroadcast();
         require(_scriptData.chainId == block.chainid, "Incorrect chainId");
-        AirlockMultisig multisig = new AirlockMultisig(Airlock(payable(_scriptData.airlock)), _scriptData.signers);
+        AirlockMultisig multisig = new AirlockMultisig(Airlock(payable(_scriptData.airlock)), DopplerHookInitializer(_scriptData.dopplerHookInitializer), _scriptData.signers);
         vm.stopBroadcast();
     }
 }
@@ -33,7 +35,10 @@ contract DeployAirlockMultisigBaseSepoliaScript is DeployAirlockMultisigScript {
         signers[1] = 0x88C23B886580FfAd04C66055edB6c777f5F74a08;
 
         _scriptData = ScriptData({
-            chainId: ChainIds.BASE_SEPOLIA, airlock: 0x3411306Ce66c9469BFF1535BA955503c4Bde1C6e, signers: signers
+            chainId: ChainIds.BASE_SEPOLIA, 
+            airlock: 0x3411306Ce66c9469BFF1535BA955503c4Bde1C6e, 
+            signers: signers, 
+            dopplerHookInitializer: 0x98cd6478debe443069db863abb9626d94de9a544
         });
     }
 }
@@ -45,7 +50,12 @@ contract DeployAirlockMultisigUnichainSepoliaScript is DeployAirlockMultisigScri
         signers[0] = msg.sender;
         signers[1] = 0x88C23B886580FfAd04C66055edB6c777f5F74a08;
 
-        _scriptData = ScriptData({ chainId: ChainIds.UNICHAIN_SEPOLIA, airlock: address(0), signers: signers });
+        _scriptData = ScriptData({ 
+            chainId: ChainIds.UNICHAIN_SEPOLIA, 
+            airlock: address(0), 
+            signers: signers ,
+            dopplerHookInitializer: address(0)
+        });
     }
 }
 
@@ -57,7 +67,10 @@ contract DeployAirlockMultisigMonadTestnetScript is DeployAirlockMultisigScript 
         signers[1] = 0x88C23B886580FfAd04C66055edB6c777f5F74a08;
 
         _scriptData = ScriptData({
-            chainId: ChainIds.MONAD_TESTNET, airlock: 0xa82c66b6ddEb92089015C3565E05B5c9750b2d4B, signers: signers
+            chainId: ChainIds.MONAD_TESTNET, 
+            airlock: 0xa82c66b6ddEb92089015C3565E05B5c9750b2d4B, 
+            signers: signers,
+            dopplerHookInitializer: address(0)
         });
     }
 }
