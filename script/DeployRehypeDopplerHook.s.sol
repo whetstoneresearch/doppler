30d9d9c179e19a398b97baefcef61baa74a866fe
clemlak <39790678+clemlak@users.noreply.github.com>
2026-01-05 19:59:26 +0400

    build: add missing imports

--- script/DeployRehypeDopplerHook.s.sol
+++ script/DeployRehypeDopplerHook.s.sol
@@ -1,8 +1,10 @@
 // SPDX-License-Identifier: UNLICENSED
 pragma solidity ^0.8.24;
 
+import { Config } from "forge-std/Config.sol";
 import { Script } from "forge-std/Script.sol";
 import { console } from "forge-std/console.sol";
+import { ICreateX } from "script/ICreateX.sol";
 import { RehypeDopplerHook } from "src/dopplerHooks/RehypeDopplerHook.sol";
 
 contract DeployDopplerHookInitializerScript is Script, Config {
@@ -22,7 +24,7 @@ contract DeployDopplerHookInitializerScript is Script, Config {
         address dopplerHookInitializer = config.get("doppler_hook_initializer").toAddress();
         address poolManager = config.get("uniswap_v4_pool_manager").toAddress();
 
-        bytes32 salt = bytes32(0xbeefbeef);
+        bytes32 salt = bytes32(hex"beefbeef");
 
         vm.startBroadcast();
         address rehypeDopplerHook = ICreateX(createX)
