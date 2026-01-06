0fda0c0263ef90fa2222337401dafc75daae99a4
clemlak <39790678+clemlak@users.noreply.github.com>
2026-01-06 14:07:30 +0400

    build: update DeployRehypeHookScript deployment script

--- script/DeployRehypeDopplerHook.s.sol
+++ script/DeployRehypeDopplerHook.s.sol
@@ -6,8 +6,9 @@ import { Script } from "forge-std/Script.sol";
 import { console } from "forge-std/console.sol";
 import { ICreateX } from "script/ICreateX.sol";
 import { RehypeDopplerHook } from "src/dopplerHooks/RehypeDopplerHook.sol";
+import { computeCreate3Address, efficientHash } from "test/shared/AirlockMiner.sol";
 
-contract DeployDopplerHookInitializerScript is Script, Config {
+contract DeployRehypeHookScript is Script, Config {
     function run() public {
         _loadConfigAndForks("./deployments.config.toml", true);
 
@@ -20,18 +21,26 @@ contract DeployDopplerHookInitializerScript is Script, Config {
     function deployToChain(uint256 chainId) internal {
         vm.selectFork(forkOf[chainId]);
 
+        if (config.get("is_testnet").toBool() == false) {
+            return;
+        }
+
         address createX = config.get("create_x").toAddress();
         address dopplerHookInitializer = config.get("doppler_hook_initializer").toAddress();
         address poolManager = config.get("uniswap_v4_pool_manager").toAddress();
 
-        bytes32 salt = bytes32(hex"beefbeef");
-
         vm.startBroadcast();
+        bytes32 salt = bytes32(uint256(uint160(msg.sender)) << 96 | 0x12345);
+        address expectedAddress = computeCreate3Address(
+            efficientHash({ a: bytes32(uint256(uint160(msg.sender))), b: salt }), address(createX)
+        );
+
         address rehypeDopplerHook = ICreateX(createX)
             .deployCreate3(
                 salt,
                 abi.encodePacked(type(RehypeDopplerHook).creationCode, abi.encode(dopplerHookInitializer, poolManager))
             );
+        require(rehypeDopplerHook == expectedAddress, "Unexpected deployed address");
 
         console.log("rehypeDopplerHook deployed to:", rehypeDopplerHook);
         config.set("rehype_doppler_hook", rehypeDopplerHook);
