e462e93213b7b921f7329897fd0468cc6625e436
clemlak <39790678+clemlak@users.noreply.github.com>
2026-01-19 11:37:06 +0400

    build: update DeployRehypeHookScript

--- script/DeployRehypeDopplerHook.s.sol
+++ script/DeployRehypeDopplerHook.s.sol
@@ -5,15 +5,19 @@ import { Config } from "forge-std/Config.sol";
 import { Script } from "forge-std/Script.sol";
 import { console } from "forge-std/console.sol";
 import { ICreateX } from "script/ICreateX.sol";
+import { computeCreate3Address, computeCreate3GuardedSalt, generateCreate3Salt } from "script/utils/CreateX.sol";
 import { RehypeDopplerHook } from "src/dopplerHooks/RehypeDopplerHook.sol";
-import { computeCreate3Address, efficientHash } from "test/shared/AirlockMiner.sol";
 
 contract DeployRehypeHookScript is Script, Config {
     function run() public {
         _loadConfigAndForks("./deployments.config.toml", true);
 
-        for (uint256 i; i < chainIds.length; i++) {
-            uint256 chainId = chainIds[i];
+        uint256[] memory targets = new uint256[](1);
+        targets[0] = 84_532;
+        targets[1] = 1301;
+
+        for (uint256 i; i < targets.length; i++) {
+            uint256 chainId = targets[i];
             deployToChain(chainId);
         }
     }
@@ -21,20 +25,13 @@ contract DeployRehypeHookScript is Script, Config {
     function deployToChain(uint256 chainId) internal {
         vm.selectFork(forkOf[chainId]);
 
-        // TODO: Remove this to deploy to production
-        if (config.get("is_testnet").toBool() == false) {
-            return;
-        }
-
         address createX = config.get("create_x").toAddress();
         address dopplerHookInitializer = config.get("doppler_hook_initializer").toAddress();
         address poolManager = config.get("uniswap_v4_pool_manager").toAddress();
 
         vm.startBroadcast();
-        bytes32 salt = bytes32(uint256(uint160(msg.sender)) << 96 | 0x12345);
-        address expectedAddress = computeCreate3Address(
-            efficientHash({ a: bytes32(uint256(uint160(msg.sender))), b: salt }), address(createX)
-        );
+        bytes32 salt = generateCreate3Salt(msg.sender, type(RehypeDopplerHook).name);
+        address expectedAddress = computeCreate3Address(computeCreate3GuardedSalt(salt, msg.sender), address(createX));
 
         address rehypeDopplerHook = ICreateX(createX)
             .deployCreate3(
