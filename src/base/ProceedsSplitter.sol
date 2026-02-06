4e4f0f2d1f9dc60e657948d2e2b40cfc884fa58e
clemlak <39790678+clemlak@users.noreply.github.com>
2026-02-06 19:53:00 +0400

    chore: add NatSpec to ProceedsSplitter

--- src/base/ProceedsSplitter.sol
+++ src/base/ProceedsSplitter.sol
@@ -14,28 +14,62 @@ struct SplitConfiguration {
 /// @dev Maximum share that can be allocated (50%)
 uint256 constant MAX_SPLIT_SHARE = 0.5e18;
 
+/// @notice Thrown when the recipient of the split is the zero address
 error InvalidSplitRecipient();
 
+/// @notice Thrown when the share of the split is higher than the maximum allowed
 error SplitShareTooHigh(uint256 actual, uint256 maximum);
 
+/**
+ * @notice Emitted when the proceeds split is distributed for an asset / numeraire pair
+ * @param token0 Address of token0 of the pair
+ * @param token1 Address of token1 of the pair
+ * @param recipient Address of the recipient of the split
+ * @param amount Amount of the split distributed (in numeraire)
+ */
 event DistributeSplit(address indexed token0, address indexed token1, address indexed recipient, uint256 amount);
 
+/**
+ * @title ProceedsSplitter base contract
+ * @author Whetstone Research
+ * @custom:security-contact security@whetstone.cc
+ * @dev Base contract to be inherited by the migrators to implement the proceeds split logic
+ */
 abstract contract ProceedsSplitter {
+    /// @notice Address of the TopUpDistributor contract
     TopUpDistributor public immutable TOP_UP_DISTRIBUTOR;
 
     /// @notice Configuration of proceeds split, stored by asset since they are unique
     mapping(address token0 => mapping(address token1 => SplitConfiguration config)) public splitConfigurationOf;
 
+    /// @param topUpDistributor Address of the TopUpDistributor contract
     constructor(TopUpDistributor topUpDistributor) {
         TOP_UP_DISTRIBUTOR = topUpDistributor;
     }
 
+    /**
+     * @dev Sets the proceeds split configuration for an asset / numeraire pair, ordered in ascending
+     * order (token0 < token1)
+     * @param token0 Address of token0 of the pair
+     * @param token1 Address of token1 of the pair
+     * @param config Split configuration to set for the pair
+     */
     function _setSplit(address token0, address token1, SplitConfiguration memory config) internal {
         require(config.recipient != address(0), InvalidSplitRecipient());
         require(config.share <= MAX_SPLIT_SHARE, SplitShareTooHigh(config.share, MAX_SPLIT_SHARE));
         splitConfigurationOf[token0][token1] = config;
     }
 
+    /**
+     * @dev Distributes the proceeds split for an asset / numeraire pair, ordered in ascending
+     * order (token0 < token1), note that only the numeraire balance is split and transferred
+     * @param token0 Address of token0 of the pair
+     * @param token1 Address of token1 of the pair
+     * @param balance0 Balance of token0 to split
+     * @param balance1 Balance of token1 to split
+     * @return balanceLeft0 Balance of token0 left after the split
+     * @return balanceLeft1 Balance of token1 left after the split
+     */
     function _distributeSplit(
         address token0,
         address token1,
