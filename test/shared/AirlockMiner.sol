47ea12967d4cec9a1da834d94c6282c92a231254
clemlak <39790678+clemlak@users.noreply.github.com>
2026-03-06 10:49:07 +0400

    build: update DopplerHookInitializer mining script

--- test/shared/AirlockMiner.sol
+++ test/shared/AirlockMiner.sol
@@ -83,7 +83,7 @@ function mineDecayMulticurveInitializer(address sender, address deployer) view r
 
 uint160 constant DOPPLER_HOOK_INITIALIZER_FLAGS = uint160(
     Hooks.BEFORE_INITIALIZE_FLAG | Hooks.AFTER_ADD_LIQUIDITY_FLAG | Hooks.AFTER_REMOVE_LIQUIDITY_FLAG
-        | Hooks.AFTER_SWAP_FLAG | Hooks.AFTER_SWAP_RETURNS_DELTA_FLAG
+        | Hooks.BEFORE_SWAP_FLAG | Hooks.AFTER_SWAP_FLAG | Hooks.AFTER_SWAP_RETURNS_DELTA_FLAG
 );
 
 struct MineDopplerHookInitializerParams {
